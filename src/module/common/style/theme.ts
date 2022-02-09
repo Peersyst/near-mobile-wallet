d4bbbf61f9098eeff42baa85220f0e4eeef3a5d6
JordiParraCrespo <jordiparra99@gmail.com>
2022-02-09 17:34:24 +0100

    pallete added

--- src/module/common/style/theme.ts
+++ src/module/common/style/theme.ts
@@ -2,6 +2,19 @@ import { Theme } from "./theme.types";
 
 export const theme: Theme = {
     palette: {
-        primary: "cyan",
+        primary: "#623EDF",
+        white: "#FFFFFF",
+        black: "#0B0D1E",
+        darkGray: "#707070",
+        turquoise: "#15C8BD",
+        gold: "#E4AF4C",
+        violet: "#924AD9",
+        pink: "#FF66B0",
+        blue: "#47B5D6",
+        purple: "#623EDF",
+        darkerGray: "#141414",
+        gray: "#00000069",
+        lightGray: "#F9F9F966",
+        red: "#FF0E0E",
     },
 };
