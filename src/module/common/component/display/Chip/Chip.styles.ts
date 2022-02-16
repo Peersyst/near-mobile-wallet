34298576ff1843d5161439e7bc8ff1d3552a8ea8
JordiParraCrespo <jordiparra99@gmail.com>
2022-02-16 12:20:20 +0100

    linting

--- src/module/common/component/display/Chip/Chip.styles.ts
+++ src/module/common/component/display/Chip/Chip.styles.ts
@@ -3,8 +3,7 @@ import styled from "@peersyst/react-native-styled";
 import { View, Text } from "react-native";
 import { AppearanceProps } from "module/common/types";
 
-export const ChipRoot = styled(View)<ChipRootProps>(({ theme, appearance, fullWidth }) =>
-({
+export const ChipRoot = styled(View)<ChipRootProps>(({ theme, appearance, fullWidth }) => ({
     height: 48,
     paddingHorizontal: 22,
     alignItems: "center",
@@ -19,11 +18,10 @@ export const ChipRoot = styled(View)<ChipRootProps>(({ theme, appearance, fullWi
     shadowRadius: 10,
     elevation: 7,
     alignSelf: fullWidth ? undefined : "flex-start",
-    backgroundColor: appearance === "light" ? (theme.palette.lighterGray) : (theme.palette.black)
-}))
+    backgroundColor: appearance === "light" ? theme.palette.lighterGray : theme.palette.black,
+}));
 
-export const ChipText = styled(Text)<AppearanceProps>(({ theme, appearance }) =>
-({
+export const ChipText = styled(Text)<AppearanceProps>(({ theme, appearance }) => ({
     fontSize: 18,
-    color: appearance === "light" ? theme.palette.darkFont : theme.palette.white
-}))
+    color: appearance === "light" ? theme.palette.darkFont : theme.palette.white,
+}));
