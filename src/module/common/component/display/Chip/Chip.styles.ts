0710892798add987e1b127121f9e932f337cfbe1
JordiParraCrespo <jordiparra99@gmail.com>
2022-02-16 17:27:55 +0100

    Design updated

--- src/module/common/component/display/Chip/Chip.styles.ts
+++ src/module/common/component/display/Chip/Chip.styles.ts
@@ -4,24 +4,24 @@ import { View, Text } from "react-native";
 import { AppearanceProps } from "module/common/types";
 
 export const ChipRoot = styled(View)<ChipRootProps>(({ theme, appearance, fullWidth }) => ({
-    height: 48,
-    paddingHorizontal: 22,
+    height: 30,
+    paddingHorizontal: 20,
     alignItems: "center",
     justifyContent: "center",
     borderRadius: theme.borderRadius,
     shadowColor: theme.palette.fullBlack,
     shadowOffset: {
         width: 0,
-        height: 1,
+        height: 2,
     },
     shadowOpacity: 0.4,
     shadowRadius: 10,
-    elevation: 7,
+    elevation: 6,
     alignSelf: fullWidth ? undefined : "flex-start",
     backgroundColor: appearance === "light" ? theme.palette.lighterGray : theme.palette.black,
 }));
 
 export const ChipText = styled(Text)<AppearanceProps>(({ theme, appearance }) => ({
-    fontSize: 18,
+    fontSize: 14,
     color: appearance === "light" ? theme.palette.darkFont : theme.palette.white,
 }));
