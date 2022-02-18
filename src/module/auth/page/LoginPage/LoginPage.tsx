6395e8d3491cf555f476e5ebd5c5ed1e03f50dec
JordiParraCrespo <jordiparra99@gmail.com>
2022-02-18 15:55:40 +0100

    PR To be revised

--- src/module/auth/page/LoginPage/LoginPage.tsx
+++ src/module/auth/page/LoginPage/LoginPage.tsx
@@ -2,16 +2,17 @@ import NumericPad from "module/common/component/input/NumericPad/NumericPad";
 import { SafeAreaView, StatusBar } from "react-native";
 
 const LoginPage = (): JSX.Element => {
-    //const [password, setPassword] = useState("");
 
     return (
         <SafeAreaView
+            //Temporary styles to see the page
             style={{
                 marginTop: StatusBar.currentHeight,
                 flexDirection: "column",
                 alignItems: "center",
                 justifyContent: "center",
                 paddingTop:"57%",
+                paddingHorizontal: 20,
                 backgroundColor: "black",
                 height: "100%",
             }}
--- src/module/common/component/layout/Keyboard/Keyboard.styles.ts
+++ src/module/common/component/layout/Keyboard/Keyboard.styles.ts
@@ -2,7 +2,6 @@ import styled from "@peersyst/react-native-styled"
 import { Row } from "react-native-components"
 
 export const KeyboardRoot = styled(Row, { gap: 2 })(() => ({
-    width: 280,
     flex: 1,
     flexDirection: "row",
     justifyContent: "space-between",
