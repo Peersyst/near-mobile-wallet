aaacf6289d6f50de0fdd6663518d685007999ac1
JordiParraCrespo <jordiparra99@gmail.com>
2022-03-07 09:23:24 +0100

    Paginated with onViawableItems working =)

--- src/module/main/screen/HomePage/HomePage.tsx
+++ src/module/main/screen/HomePage/HomePage.tsx
@@ -1,8 +1,20 @@
 import Button from "module/common/component/input/Button/Button";
 import BasePage from "module/common/component/layout/BasePage/BasePage";
 import { useAuth } from "module/auth/hook/useAuth";
-import { View, Text, ScrollView, Dimensions, FlatList, StyleSheet, NativeScrollEvent, Animated } from "react-native";
+import {
+    View,
+    Text,
+    ScrollView,
+    Dimensions,
+    FlatList,
+    StyleSheet,
+    NativeScrollEvent,
+    Animated,
+    ViewToken,
+    useWindowDimensions,
+} from "react-native";
 import { createRef, useEffect, useRef, useState } from "react";
+import { Row } from "react-native-components";
 const DATA = [
     {
         id: "0",
@@ -27,35 +39,58 @@ const Item = ({ item }: ItemProps): JSX.Element => {
     const flattenStyle = StyleSheet.flatten([styles.item, item.id === "1" && styles.item1, item.id === "2" && styles.item2]);
     return <View style={flattenStyle} />;
 };
+interface PaginatorProps {
+    scrollX: Animated.Value;
+}
+const Paginator = ({ scrollX }: PaginatorProps) => {
+    const { width } = useWindowDimensions();
+
+    return (
+        <View style={{ flexDirection: "row", height: 64 }}>
+            {DATA.map((_, i) => {
+                //PreviousDot, currentDot, nextDot
+                const inputRange = [(i - 1) * width, i * width, (i + 1) * width];
+
+                const dotWidth = scrollX.interpolate({
+                    inputRange,
+                    outputRange: [10, 20, 10],
+                    extrapolate: "clamp",
+                });
+
+                const opacity = scrollX.interpolate({
+                    inputRange,
+                    outputRange: [0.3, 1, 0.3],
+                    extrapolate: "clamp",
+                });
+
+                return (
+                    <Animated.View
+                        style={[
+                            styles.dot,
+                            {
+                                width: dotWidth,
+                                opacity,
+                            },
+                        ]}
+                        key={i.toString()}
+                    />
+                );
+            })}
+        </View>
+    );
+};
 
 const HomePage = (): JSX.Element => {
     const { logout } = useAuth();
-    const [activeIndex, setActiveIndex] = useState<number>();
+    const [activeIndex, setActiveIndex] = useState<number>(0);
     const flatListRef = createRef<FlatList<any>>();
-
-    // const onChange = (nativeEvent: NativeScrollEvent) => {
-    //     const offset = nativeEvent.contentOffset.x;
-    //     const layoutWidth = nativeEvent.layoutMeasurement.width;
-    //     const slide: number = Math.ceil(offset / layoutWidth);
-    //     console.log("1");
-    //     if (slide !== activeIndex) {
-    //         setActiveIndex(slide);
-    //     }
-    // };
-
-    // const onDragEnd = (nativeEvent: NativeScrollEvent) => {
-    //     // if (activeIndex === 2 && nativeEvent.velocity && nativeEvent.velocity.x < -1) {
-    //     //     flatListRef.current?.scrollToOffset({ animated: true, offset: 0 });
-    //     // }
-    //     // const offset = nativeEvent.contentOffset.x;
-    //     // const layoutWidth = nativeEvent.layoutMeasurement.width;
-    //     // const slide: number = Math.ceil(offset / layoutWidth);
-    //     // if (slide !== activeIndex) {
-    //     //     setActiveIndex(slide);
-    //     // }
-    // };
-
     const scrollX = useRef(new Animated.Value(0)).current;
+
+    const onViewRef = useRef((info: { viewableItems: ViewToken[]; changed: ViewToken[] }) => {
+        if (info && info.viewableItems[0]) setActiveIndex(info.viewableItems[0]?.index || 0);
+    });
+
+    const viewConfig = useRef({ waitForInteraction: true, viewAreaCoveragePercentThreshold: 50 });
     return (
         <BasePage appearance="light" showIcons>
             <Button onPress={logout}>Log out</Button>
@@ -63,9 +98,6 @@ const HomePage = (): JSX.Element => {
                 <FlatList
                     ref={flatListRef}
                     horizontal
-                    // onScroll={({ nativeEvent }) => onChange(nativeEvent)}
-                    // onScrollBeginDrag={({ nativeEvent }) => console.log("Start", nativeEvent.velocity?.x)}
-                    //onScrollEndDrag={({ nativeEvent }) => onDragEnd(nativeEvent)}
                     showsHorizontalScrollIndicator={false}
                     pagingEnabled
                     renderItem={({ item }) => {
@@ -75,11 +107,16 @@ const HomePage = (): JSX.Element => {
                         useNativeDriver: false,
                     })}
                     bounces={false}
+                    scrollEventThrottle={32}
                     keyExtractor={(item) => item.title}
                     data={DATA}
+                    onViewableItemsChanged={onViewRef.current}
+                    viewabilityConfig={viewConfig.current}
                 />
             </View>
-            <Text>{activeIndex}</Text>
+            <Row justifyContent="center" alignItems="center"> 
+                <Paginator scrollX={scrollX} />
+            </Row>
         </BasePage>
     );
 };
@@ -102,6 +139,12 @@ const styles = StyleSheet.create({
     },
     title: {
         fontSize: 32,
+    },
+    dot: {
+        height: 10,
+        borderRadius: 5,
+        backgroundColor: "#000",
+        marginHorizontal: 4,
     },
 });
 export default HomePage;
