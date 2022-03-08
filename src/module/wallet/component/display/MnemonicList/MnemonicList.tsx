bcdca2a94aee7b47374e5adc7ca92416bfb94bce
AgustinMJ <74896585+AgustinMJ@users.noreply.github.com>
2022-03-08 08:40:30 +0100

    mnemonic list overflow fixed

--- src/module/wallet/component/display/MnemonicList/MnemonicList.tsx
+++ src/module/wallet/component/display/MnemonicList/MnemonicList.tsx
@@ -1,6 +1,6 @@
 import { ChipProps } from "module/common/component/display/Chip/Chip.types";
 import Chip from "module/common/component/display/Chip/Chip";
-import { FlatList } from "react-native";
+import { Col, Row } from "react-native-components";
 
 export interface MnemonicListProps {
     mnemonic: string[];
@@ -8,17 +8,26 @@ export interface MnemonicListProps {
     onPress?: (item: string) => unknown;
 }
 
-const MnemonicList = ({ mnemonic, appearance = "light", onPress }: MnemonicListProps): JSX.Element => (
-    <FlatList
-        data={mnemonic}
-        renderItem={({ item, index }) => (
-            <Chip label={item} style={{ marginLeft: index % 3 === 0 ? 0 : 15 }} appearance={appearance} onPress={() => onPress?.(item)} />
-        )}
-        keyExtractor={(item) => item}
-        style={{ overflow: "visible" }}
-        numColumns={3}
-        columnWrapperStyle={{ marginBottom: 15, justifyContent: "center" }}
-    />
-);
+const MnemonicList = ({ mnemonic, appearance = "light", onPress }: MnemonicListProps): JSX.Element => {
+    const rows = Math.ceil(mnemonic.length / 3);
+    return (
+        <Col gap={15} alignItems="center">
+            {[...Array(rows)].map((_, i) => {
+                const j = i * 3;
+                return (
+                    <Row gap={15} key={i}>
+                        {mnemonic[j] && <Chip label={mnemonic[j]} appearance={appearance} onPress={() => onPress?.(mnemonic[j])} />}
+                        {mnemonic[j + 1] && (
+                            <Chip label={mnemonic[j + 1]} appearance={appearance} onPress={() => onPress?.(mnemonic[j + 1])} />
+                        )}
+                        {mnemonic[j + 2] && (
+                            <Chip label={mnemonic[j + 2]} appearance={appearance} onPress={() => onPress?.(mnemonic[j + 2])} />
+                        )}
+                    </Row>
+                );
+            })}
+        </Col>
+    );
+};
 
 export default MnemonicList;
