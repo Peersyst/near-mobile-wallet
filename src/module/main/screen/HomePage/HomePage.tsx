8ae60cadda402c3e9b0d9045c5ad9aacdcb4f381
JordiParraCrespo <jordiparra99@gmail.com>
2022-03-10 12:32:18 +0100

    FInished

--- src/module/main/screen/HomePage/HomePage.tsx
+++ src/module/main/screen/HomePage/HomePage.tsx
@@ -6,62 +6,22 @@ import { WalletStorage } from "module/wallet/WalletStorage";
 import { Col } from "react-native-components";
 import MainSlider from "module/main/component/core/MainSlider";
 import { MAIN_SCREEN_PADDING } from "module/main/MainNavigatorGroup";
-import {View, StyleSheet} from "react-native";
-
-const DATA = [
-    {
-        id: "0",
-        title: "First Item",
-    },
-    {
-        id: "1",
-        title: "Second Item",
-    },
-    {
-        id: "2",
-        title: "Third Item",
-    },
-    {
-        id: "3",
-        title: "Third Item",
-    },
-    {
-        id: "4",
-        title: "Third Item",
-    },
-    {
-        id: "5",
-        title: "Third Item",
-    },
-];
-interface ItemProps {
-    item: {
-        id?: string;
-        title?: string;
-    };
-}
-const Item = ({ item }: ItemProps): JSX.Element => {
-    const flattenStyle = StyleSheet.flatten([styles.item, item.id === "1" && styles.item1, item.id === "2" && styles.item2]);
-    return <View style={flattenStyle} />;
-};
-
 
 const HomePage = (): JSX.Element => {
     const setWalletState = useSetRecoilState(walletState);
     return (
         <BasePage appearance="light" showIcons>
-            <Button onPress={() => setWalletState((state) => ({ ...state, isAuthenticated: false }))}>Log out</Button>
-            <Button
-                onPress={async () => {
-                    await WalletStorage.clear();
-                    setWalletState({ isAuthenticated: false, hasWallet: false, name: undefined });
-                }}
-            >
-                Erase
-            </Button>
             <MainSlider />
             <Col style={{ paddingHorizontal: MAIN_SCREEN_PADDING }}>
                 <Button onPress={() => setWalletState((state) => ({ ...state, isAuthenticated: false }))}>Log out</Button>
+                <Button
+                    onPress={async () => {
+                        await WalletStorage.clear();
+                        setWalletState({ hasWallet: false, isAuthenticated: false, isFirstTime: false, accounts: [] });
+                    }}
+                >
+                    Erase
+                </Button>
             </Col>
         </BasePage>
     );
