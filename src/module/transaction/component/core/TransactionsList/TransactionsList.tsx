8077524eaeff359c39b68fdf7073e3b06ac87a6e
JordiParraCrespo <jordiparra99@gmail.com>
2022-03-14 12:09:34 +0100

    transactions test and no component updated

--- src/module/transaction/component/core/TransactionsList/TransactionsList.tsx
+++ src/module/transaction/component/core/TransactionsList/TransactionsList.tsx
@@ -3,6 +3,12 @@ import { FlatList, ScrollView } from "react-native";
 import useGetTransactions from "module/transaction/query/useGetTransactions";
 import Divider from "module/common/component/display/Divider/Divider";
 import useWallet from "module/wallet/hook/useWallet";
+import { Typography } from "react-native-components";
+import { translate } from "locale";
+
+const NoTransactionsComponent = (): JSX.Element => {
+    return <Typography variant="body1">{translate("no_transactions")}</Typography>;
+};
 
 const TransactionsList = (): JSX.Element => {
     const {
@@ -12,14 +18,16 @@ const TransactionsList = (): JSX.Element => {
         data = [],
         refetch,
         isFetching,
+        isLoading,
     } = useGetTransactions(selectedAccount !== undefined ? cells[selectedAccount].address : undefined);
 
     return (
         <ScrollView style={{ flex: 1 }} contentContainerStyle={{ flex: 1 }} horizontal scrollEnabled={false}>
             <FlatList
                 onRefresh={refetch}
-                refreshing={isFetching}
+                refreshing={isFetching || isLoading}
                 data={data}
+                ListEmptyComponent={isLoading ? undefined : NoTransactionsComponent}
                 renderItem={({ item: tx }) => <TransactionCard {...tx} />}
                 keyExtractor={(tx) => tx.transactionHash}
                 ItemSeparatorComponent={() => <Divider width="full-width" />}
--- test/unit/src/module/transaction/component/core/TransactionsList/TransactionsList.spec.tsx
+++ test/unit/src/module/transaction/component/core/TransactionsList/TransactionsList.spec.tsx
@@ -4,32 +4,26 @@ import { transactions } from "mocks/transaction";
 import { render, SuccessApiCall } from "test-utils";
 import { waitFor } from "@testing-library/react-native";
 import TransactionsList from "module/transaction/component/core/TransactionsList/TransactionsList";
+import { translate } from "locale";
+import { mockedUseWallet } from "mocks/useWallet";
 
 describe("TransactionsList tests", () => {
     afterEach(() => {
         jest.restoreAllMocks();
     });
-
     test("Renders correctly with an account", async () => {
-        jest.spyOn(UseWallet, "default").mockReturnValue({
-            state: {
-                hasWallet: true,
-                isAuthenticated: true,
-                isFirstTime: false,
-                cells: [{ address: "address", balance: "1" }],
-                selectedAccount: 0,
-            },
-            setAuthenticated: jest.fn(),
-            setCells: jest.fn(),
-            setSelectedAccount: jest.fn(),
-            setState: jest.fn(),
-            reset: jest.fn(),
-        });
+        jest.spyOn(UseWallet, "default").mockReturnValue(mockedUseWallet);
         jest.spyOn(GetTransactions, "default").mockReturnValue(SuccessApiCall(transactions));
 
         const screen = render(<TransactionsList />);
         await waitFor(() => expect(screen.getByText("01/01/2022 - 00:00")));
         expect(screen.getByText("02/01/2022 - 00:00"));
         expect(screen.getByText("03/01/2022 - 00:00"));
+    });
+    test("Renders correctly with an account", async () => {
+        jest.spyOn(UseWallet, "default").mockReturnValue(mockedUseWallet);
+        jest.spyOn(GetTransactions, "default").mockReturnValue(SuccessApiCall([]));
+        const screen = render(<TransactionsList />);
+        await waitFor(() => expect(screen.getAllByText(translate("no_transactions"))));
     });
 });
