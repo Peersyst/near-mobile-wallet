32764c0d9dc13f7bdb3950dd11e6db9f37ef2a13
AgustinMJ <74896585+AgustinMJ@users.noreply.github.com>
2022-03-17 13:02:13 +0100

    QrScanner props fix

--- src/module/common/component/base/input/QrScanner/QrScanner.types.ts
+++ src/module/common/component/base/input/QrScanner/QrScanner.types.ts
@@ -1,9 +1,9 @@
-import { BarCodeScannerProps } from "expo-barcode-scanner";
+import { CameraProps } from "expo-camera";
 import { ReactElement } from "react";
 import { BackdropProps } from "module/common/component/base";
 
 export interface QrScannerProps
     extends Omit<BackdropProps, "children" | "transitionsDuration" | "transparent" | "closable" | "defaultOpen"> {
     back?: ReactElement;
-    onScan: NonNullable<BarCodeScannerProps["onBarCodeScanned"]>;
+    onScan: NonNullable<CameraProps["onBarCodeScanned"]>;
 }
