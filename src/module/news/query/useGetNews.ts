2e3fe4d831d1354664699bb8fe6849e844bb3d88
JordiParraCrespo <jordiparra99@gmail.com>
2022-03-23 18:10:17 +0100

    Get the rrss provider by https

--- src/module/news/query/useGetNews.ts
+++ src/module/news/query/useGetNews.ts
@@ -2,9 +2,11 @@ import { XMLParser } from "fast-xml-parser";
 import { QueryResult } from "query-utils";
 import { useQuery } from "react-query";
 
+const rrssUriProvider = "https://fetchrss.com/rss/6239aa2ceb62c371b8448ee26239aa120841546894777912.xml"
+
 const useGetNews = (): QueryResult<any[]> =>
     useQuery(["news"], async () => {
-        const res: Response = await fetch("http://fetchrss.com/rss/6239aa2ceb62c371b8448ee26239aa120841546894777912.xml");
+        const res: Response = await fetch(rrssUriProvider);
         const data = await res.text();
         const options = {
             ignoreDeclaration: true,
