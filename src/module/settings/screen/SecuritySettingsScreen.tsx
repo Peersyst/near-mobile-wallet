b62f3332a5c91e67c221f93dd6f4712ceb3149bb
JordiParraCrespo <jordiparra99@gmail.com>
2022-03-28 09:22:50 +0200

    In progress

--- src/module/settings/screen/SecuritySettingsScreen.tsx
+++ src/module/settings/screen/SecuritySettingsScreen.tsx
@@ -2,7 +2,6 @@ import { translate } from "locale";
 import Button from "module/common/component/input/Button/Button";
 import BaseSecondaryScreen from "module/common/component/layout/BaseSecondaryScreen/BaseSecondaryScreen";
 import { BottomTabScreenNavigatonProps } from "module/main/component/navigation/MainBottomNavigatorGroup/MainBottomNavigatorGroup.types";
-import SendModal from "module/transaction/component/core/SendModal/SendModal";
 import walletState from "module/wallet/state/WalletState";
 import { WalletStorage } from "module/wallet/WalletStorage";
 import { Col, useModal } from "react-native-components";
@@ -11,24 +10,8 @@ import UpdatePinScreen from "./UpdatePinScreen";
 
 const SecuritySettingsScreen = ({ navigation }: BottomTabScreenNavigatonProps): JSX.Element => {
     const setWalletState = useSetRecoilState(walletState);
-    // const {
-    //     state: { pin },
-    // } = useCreateWallet();
-    // const [pinConfirmed, setPinConfirmedState] = useRecoilState(pinConfirmedState);
-    // const { showToast } = useToast();
-
-    // useEffect(() => {
-    //     const updatePin = async () => {
-    //         const storedWallet = await WalletStorage.get();
-    //         await WalletStorage.set({ ...storedWallet!, pin: pin! });
-    //         setPinConfirmedState({ pinConfirmed: false, hasNewPin: false });
-    //         showToast(translate("pin_updated_successfully"), { type: "success" });
-    //     };
-    //     if (pinConfirmed.pinConfirmed && pinConfirmed.hasNewPin) updatePin();
-    // }, [pinConfirmed]);
-
     const { showModal } = useModal();
-    
+
     return (
         <BaseSecondaryScreen navigation={navigation} title={translate("security_settings")} back={true}>
             <Col gap={20}>
--- src/module/settings/screen/UpdatePinScreen.tsx
+++ src/module/settings/screen/UpdatePinScreen.tsx
@@ -3,9 +3,23 @@ import SetWalletPinScreen from "module/wallet/screen/SetWalletPinScreen";
 import { createBackdrop, ExposedBackdropProps } from "react-native-components";
 import BaseSettingsModal from "../components/layout/BaseSettingsModal/BaseSettingsModal";
 
-const UpdatePinScreen = createBackdrop(({...rest }: ExposedBackdropProps) => {
+const UpdatePinScreen = createBackdrop(({ ...rest }: ExposedBackdropProps) => {
     const handleSubmit = () => {
         //Pin sucessfully updated
+        // const {
+        //     state: { pin },
+        // } = useCreateWallet();
+        // const [pinConfirmed, setPinConfirmedState] = useRecoilState(pinConfirmedState);
+        // const { showToast } = useToast();
+        // useEffect(() => {
+        //     const updatePin = async () => {
+        //         const storedWallet = await WalletStorage.get();
+        //         await WalletStorage.set({ ...storedWallet!, pin: pin! });
+        //         setPinConfirmedState({ pinConfirmed: false, hasNewPin: false });
+        //         showToast(translate("pin_updated_successfully"), { type: "success" });
+        //     };
+        //     if (pinConfirmed.pinConfirmed && pinConfirmed.hasNewPin) updatePin();
+        // }, [pinConfirmed]);
     };
     return (
         <BaseSettingsModal title={translate("update_your_pin")} {...rest}>
