61824907ad6bfc2b94bdc770d0a83daf796de3fe
AgustinMJ <74896585+AgustinMJ@users.noreply.github.com>
2022-03-28 09:20:41 +0200

    Fixed Skeleton

--- src/module/common/component/base/feedback/Skeleton/Skeleton.tsx
+++ src/module/common/component/base/feedback/Skeleton/Skeleton.tsx
@@ -1,11 +1,9 @@
 import { SkeletonProps } from "module/common/component/base/feedback/Skeleton/Skeleton.types";
 import { SkeletonAnimation, SkeletonOverlay, SkeletonRoot } from "./Skeleton.styles";
-import { cloneElement, useEffect, useRef } from "react";
-import { Animated } from "react-native";
+import { Children, useEffect, useRef } from "react";
+import { Animated, View } from "react-native";
 
 const Skeleton = ({ width, height, shape = "stadium", loading = true, appearance, style, children: child }: SkeletonProps): JSX.Element => {
-    const { style: childStyle = {}, ...childrenProps } = child ? child.props : {};
-
     const skeletonAnimation = useRef(new Animated.Value(0)).current;
     useEffect(() => {
         Animated.loop(
@@ -29,7 +27,7 @@ const Skeleton = ({ width, height, shape = "stadium", loading = true, appearance
         <SkeletonRoot shape={shape} height={height} width={width}>
             <SkeletonOverlay appearance={appearance} />
             <SkeletonAnimation appearance={appearance} style={{ opacity: skeletonAnimation, ...style }} />
-            {child && cloneElement(child, { ...childrenProps, style: { ...childStyle, opacity: 0 } })}
+            <View style={{ opacity: 0 }}>{Children.only(child)}</View>
         </SkeletonRoot>
     ) : (
         <>{child}</>
--- src/module/news/component/display/SimpleNewsCard/SimpleNewsCard.tsx
+++ src/module/news/component/display/SimpleNewsCard/SimpleNewsCard.tsx
@@ -1,17 +1,17 @@
 import { NewDate, NewsImage, NewTitle, SimpleNewsCardRoot, TextCont } from "./SimpleNewsCard.styles";
 import { Linking, TouchableWithoutFeedback } from "react-native";
 import formatDate from "utils/formatDate";
-import { Row, withSkeleton } from "react-native-components";
+import { Row, WithSkeleton, withSkeleton } from "react-native-components";
 import { formatNews } from "module/news/utils/formatNews";
 import { NewsDto } from "module/news/types";
 
-const SimpleNewsCard = (news: NewsDto): JSX.Element => {
+const SimpleNewsCard = ({ loading, ...news }: WithSkeleton<NewsDto>): JSX.Element => {
     const { uri, title, imageUri, date } = formatNews(news);
     return (
         <TouchableWithoutFeedback onPress={() => Linking.openURL(uri)}>
             <SimpleNewsCardRoot>
                 <Row gap={"5%"} alignItems="flex-start" justifyContent="flex-start">
-                    <NewsImage source={{ uri: imageUri }} />
+                    {!loading && <NewsImage source={{ uri: imageUri }} />}
                     <TextCont>
                         <NewTitle variant="body2">{title}</NewTitle>
                     </TextCont>
