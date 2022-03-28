232a93c93c33ef762e919906806849a77fb3c9e6
JordiParraCrespo <jordiparra99@gmail.com>
2022-03-28 17:19:12 +0200

    Added illustration to createwallet

---
+++ src/module/wallet/component/display/WalletImage/WalletImage.styles.ts
@@ -0,0 +1,14 @@
+import styled from "@peersyst/react-native-styled";
+import { Image } from "react-native";
+
+const WALLET_IMAGE_RATIO = 0.25;
+
+export const WalletImage = styled(Image)(({ dimensions: { height } }) => {
+    
+    const finalSize = height > 750 ? WALLET_IMAGE_RATIO : WALLET_IMAGE_RATIO / 1.5;
+    return {
+        width: height * finalSize,
+        height: height * finalSize,
+        display: height < 650 ? "none" : undefined,
+    }
+});
--- src/module/wallet/screen/SetWalletNameScreen.tsx
+++ src/module/wallet/screen/SetWalletNameScreen.tsx
@@ -1,9 +1,12 @@
-import { Col, Form, Typography } from "react-native-components";
+import { Col, Form, Row, Typography } from "react-native-components";
 import { translate } from "locale";
 import Card from "module/common/component/surface/Card/Card";
 import TextField from "module/common/component/input/TextField/TextField";
 import Button from "module/common/component/input/Button/Button";
 import useCreateWallet from "module/wallet/hook/useCreateWallet";
+import { SendImage } from "module/transaction/screen/SendToAddressScreen/SendToAddressScreen.styles";
+import { image } from "asset/image";
+import { WalletImage } from "../component/display/WalletImage/WalletImage.styles";
 
 interface SetWalletNameForm {
     walletName: string;
@@ -24,14 +27,16 @@ const SetWalletNameScreen = ({ onSubmit, submitText }: SetWalletNameScreenProps)
         setName(walletName);
         onSubmit();
     };
-
     return (
-        <Col flex={1} gap={30} justifyContent="flex-end">
+        <Col flex={1} gap={20} justifyContent="flex-end">
+            <Col alignItems="center" justifyContent="center" flex={1}>
+                <WalletImage source={image.wallet} />
+            </Col>
             <Card>
                 <Typography variant="body1">{translate("set_wallet_name_text")}</Typography>
             </Card>
             <Form onSubmit={handleSubmit}>
-                <Col gap={60} style={{ paddingHorizontal: 20 }}>
+                <Col gap={30} style={{ paddingHorizontal: 20 }}>
                     <TextField name="walletName" defaultValue={name} placeholder={translate("wallet_name")} validators="not-null" />
                     <Button fullWidth variant="outlined">
                         {submitText}
