5ba26c2d76a06fcf93618ed797c0fb5796b4c0fb
JordiParraCrespo <jordiparra99@gmail.com>
2022-03-28 14:22:45 +0200

    CofirmPin 60%

--- test/unit/src/module/settings/screen/SecuritySettingsScreen.spec.tsx
+++ test/unit/src/module/settings/screen/SecuritySettingsScreen.spec.tsx
@@ -1,21 +1,19 @@
 import { translate } from "locale";
-import { MainScreens } from "module/main/MainNavigatorGroup";
 import SecuritySettingsScreen from "module/settings/screen/SecuritySettingsScreen";
 import { fireEvent, render } from "test-utils";
+import * as UseModal from "module/common/component/base/feedback/ModalProvider/hooks/useModal";
 
 describe("Test for the SecuritySettingsScreen", () => {
     test("Renders correctly", () => {
         const screen = render(<SecuritySettingsScreen navigation={jest.fn() as any} />);
         expect(screen.getByText(translate("change_passcode")));
     });
-    test("Navigate to confirm password", () => {
-        const mockedNavigate = jest.fn();
-        const mockedNavigation = {
-            navigate: mockedNavigate,
-        };
-        const screen = render(<SecuritySettingsScreen navigation={mockedNavigation as any} />);
+    test("Open confirm modal to update pin", () => {
+        const showModal = jest.fn();
+        jest.spyOn(UseModal, "useModal").mockReturnValue({ showModal } as any);
+        const screen = render(<SecuritySettingsScreen navigation={jest.fn() as any} />);
         const button = screen.getByText(translate("change_passcode"));
         fireEvent.press(button);
-        expect(mockedNavigate).toHaveBeenCalledWith(MainScreens.UPDATE_PIN);
+        expect(showModal).toHaveBeenCalledTimes(2);
     });
 });
