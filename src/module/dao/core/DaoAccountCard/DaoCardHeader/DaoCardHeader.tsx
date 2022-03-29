c22d8ebd9a2f77cf18048710c5a2cf3459f30746
JordiParraCrespo <jordiparra99@gmail.com>
2022-03-29 15:33:52 +0200

    DaoCardHeader

--- src/module/dao/core/DaoAccountCard/DaoCardHeader/DaoCardHeader.tsx
+++ src/module/dao/core/DaoAccountCard/DaoCardHeader/DaoCardHeader.tsx
@@ -3,10 +3,12 @@ import { Row, Typography } from "react-native-components";
 import { DaoCardIcon } from "../DaoAccountCard.styles";
 import { Linking } from "react-native";
 
+export const DAO_INFO_URL = "https://www.nervos.org/"
+
 const DaoCardHeader = (): JSX.Element => {
     return (
         <Row justifyContent="space-between">
-            <DaoCardIcon onPress={()=> Linking.openURL("https://www.nervos.org/")}>
+            <DaoCardIcon onPress={()=> Linking.openURL(DAO_INFO_URL)}>
                 <DAOIcon />
             </DaoCardIcon>
             <Typography fontWeight="bold" variant="h2">
