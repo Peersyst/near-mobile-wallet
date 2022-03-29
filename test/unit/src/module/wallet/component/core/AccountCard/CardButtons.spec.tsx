68006a8c9afb10aa557b2393314c6764617998be
JordiParraCrespo <jordiparra99@gmail.com>
2022-03-29 15:20:52 +0200

    Refactor of cardbuttons done successfully

--- test/unit/src/module/wallet/component/core/AccountCard/CardButtons.spec.tsx
+++ test/unit/src/module/wallet/component/core/AccountCard/CardButtons.spec.tsx
@@ -1,14 +1,14 @@
 import { translate } from "locale";
-import CardButtons from "module/wallet/component/core/AccountCard/CardButtons/CardButtons";
 import { render } from "test-utils";
 import { fireEvent } from "@testing-library/react-native";
 import * as UseModal from "module/common/component/base/feedback/ModalProvider/hooks/useModal";
 import SendModal from "module/transaction/component/core/SendModal/SendModal";
 import ReceiveModal from "module/transaction/component/core/ReceiveModal/ReceiveModal";
+import AccountCardButtons from "module/wallet/component/core/AccountCard/AccountCardButtons/AccountCardButtons";
 
 describe("Renders card button", () => {
     test("Renders correctly", () => {
-        const screen = render(<CardButtons />);
+        const screen = render(<AccountCardButtons />);
         expect(screen.getByText(translate("send"))).toBeDefined();
         expect(screen.getByTestId("ReceiveIcon")).toBeDefined();
         expect(screen.getByTestId("SendIcon")).toBeDefined();
@@ -17,7 +17,7 @@ describe("Renders card button", () => {
     test("Triggers correctly send button", () => {
         const showModal = jest.fn();
         jest.spyOn(UseModal, "useModal").mockReturnValue({ showModal } as any);
-        const screen = render(<CardButtons />);
+        const screen = render(<AccountCardButtons />);
         const button = screen.getByText(translate("send"));
         fireEvent.press(button);
         expect(showModal).toHaveBeenCalledWith(SendModal);
@@ -25,7 +25,7 @@ describe("Renders card button", () => {
     test("Triggers correctly receive button", () => {
         const showModal = jest.fn();
         jest.spyOn(UseModal, "useModal").mockReturnValue({ showModal } as any);
-        const screen = render(<CardButtons />);
+        const screen = render(<AccountCardButtons />);
         const button = screen.getByText(translate("receive"));
         fireEvent.press(button);
         expect(showModal).toHaveBeenCalledWith(ReceiveModal);
