d1ef07837c5786f42a8df0f1ff0a1e96b424000b
AgustinMJ <74896585+AgustinMJ@users.noreply.github.com>
2022-03-30 13:53:04 +0200

    Reduced NewsScreen code

--- src/module/news/screen/NewsScreen.styles.ts
+++ src/module/news/screen/NewsScreen.styles.ts
@@ -1,10 +1,10 @@
 import styled from "@peersyst/react-native-styled";
 import { View } from "react-native";
-import { Col } from "react-native-components";
+import { List } from "react-native-components";
 
-export const NewsScreenRoot = styled(Col, { gap: "5%" })(() => ({
+export const NewsList = styled(List)(() => ({
     paddingHorizontal: "4%",
-    paddingTop: "10%",
+    paddingTop: "5%",
 }));
 
 export const NewsSpacer = styled(View)(() => ({
--- src/module/news/screen/NewsScreen.tsx
+++ src/module/news/screen/NewsScreen.tsx
@@ -2,9 +2,9 @@ import { translate } from "locale";
 import EmptyListComponent from "module/common/component/display/EmptyListComponent/EmptyListComponent";
 import BaseMainScreen from "module/main/component/layout/BaseMainScreen/BaseMainScreen";
 import useGetNews from "module/news/query/useGetNews";
-import { Animated, List, ScrollView } from "react-native-components";
+import { Animated } from "react-native-components";
 import SimpleNewsCard from "../component/display/SimpleNewsCard/SimpleNewsCard";
-import { NewsScreenRoot, NewsSpacer } from "./NewsScreen.styles";
+import { NewsList, NewsSpacer } from "./NewsScreen.styles";
 
 const AnimatedSimpleNewsCard = Animated.createAnimatedComponent.fade(SimpleNewsCard, { duration: 300, appear: true });
 
@@ -12,23 +12,20 @@ const NewsScreen = (): JSX.Element => {
     const { data = [], refetch, isLoading } = useGetNews();
 
     return (
-        <ScrollView refreshControlProps={{ tintColor: "black" }} onRefresh={refetch}>
-            <BaseMainScreen>
-                <NewsScreenRoot>
-                    <List
-                        renderItem={({ item, index }) => {
-                            return <AnimatedSimpleNewsCard loading={isLoading} in {...item} key={index} />;
-                        }}
-                        scrollEnabled={false}
-                        keyExtractor={(_, index) => index.toString()}
-                        data={isLoading ? new Array(5).fill({}) : data}
-                        ItemSeparatorComponent={() => <NewsSpacer />}
-                        ListFooterComponent={() => <NewsSpacer style={{ paddingTop: "30%" }} />}
-                        ListEmptyComponent={isLoading ? undefined : <EmptyListComponent message={translate("no_news")} />}
-                    />
-                </NewsScreenRoot>
-            </BaseMainScreen>
-        </ScrollView>
+        <BaseMainScreen>
+            <NewsList
+                renderItem={({ item, index }) => {
+                    return <AnimatedSimpleNewsCard loading={isLoading} in {...item} key={index} />;
+                }}
+                refreshControlProps={{ tintColor: "black" }}
+                onRefresh={refetch}
+                keyExtractor={(_, index) => index.toString()}
+                data={isLoading ? new Array(5).fill({}) : data}
+                ItemSeparatorComponent={() => <NewsSpacer />}
+                ListFooterComponent={() => <NewsSpacer style={{ paddingTop: "30%" }} />}
+                ListEmptyComponent={isLoading ? undefined : <EmptyListComponent message={translate("no_news")} />}
+            />
+        </BaseMainScreen>
     );
 };
 
