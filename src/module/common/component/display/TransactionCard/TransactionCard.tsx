631a4b6755d202dd9332e41745accd063ef2ac22
JordiParraCrespo <jordiparra99@gmail.com>
2022-03-30 09:28:51 +0200

    Transaction in progress

--- src/module/common/component/display/TransactionCard/TransactionCard.tsx
+++ src/module/common/component/display/TransactionCard/TransactionCard.tsx
@@ -1,32 +1,32 @@
 import { Transaction } from "module/transaction/types";
 import { Col, Row, Typography } from "react-native-components";
-import { TransactionCardRoot, TransactionIcon } from "module/transaction/component/display/TransactionCard/TransactionCard.styles";
+import { TransactionCardRoot, TransactionIcon } from "module/transaction/component/display/MainTransactionCard/TransactionCard.styles";
 import formatDate from "utils/formatDate";
 import Balance from "module/wallet/component/display/Balance/Balance";
 import formatNumber from "utils/formatNumber";
-import { TransactionReceivedIcon, TransactionSentIcon } from "icons";
+import { ReactElement } from "react";
+import { BalanceProps } from "module/wallet/component/display/Balance/Balance.types";
 
-export type TransactionCardProps = Transaction;
-const tokens = ["CKB", "CKB", "CKB", "ETH", "USDC", "TAI", "CKB", "CKB", "COOP", "CKB"];
+export type TransactionCardProps = Transaction & {
+    TxIcon: ReactElement;
+    label: string;
+} & Pick<BalanceProps, "units" | "action">;
 
-const TransactionCard = ({ timestamp }: TransactionCardProps): JSX.Element => {
-    const seed = Math.random();
-    const received = seed > 0.5;
-    const token = tokens[Math.trunc(seed * 10)];
+const TransactionCard = ({ timestamp, TxIcon, label, units, action }: TransactionCardProps): JSX.Element => {
 
     return (
         <TransactionCardRoot>
-            <TransactionIcon>{received ? <TransactionReceivedIcon /> : <TransactionSentIcon />}</TransactionIcon>
+            <TransactionIcon>{TxIcon}</TransactionIcon>
             <Col gap={2} flex={1}>
                 <Row justifyContent="space-between">
                     <Typography variant="body1" fontWeight="bold">
-                        {received ? "Received" : "Sent"}
+                        {label}
                     </Typography>
                     <Balance
                         balance={((timestamp.getTime() * (1 + Math.random())) / Math.pow(12, 9)).toFixed(2)}
-                        units={token}
+                        units={units}
                         variant="body1"
-                        action={received ? "add" : "subtract"}
+                        action={action}
                         fontWeight="bold"
                         boldUnits
                     />
--- test/unit/src/module/transaction/component/display/TransactionCard/TransactionCard.spec.tsx
+++ test/unit/src/module/transaction/component/display/TransactionCard/TransactionCard.spec.tsx
@@ -1,10 +1,10 @@
 import { render } from "test-utils";
-import TransactionCard from "module/transaction/component/display/TransactionCard/TransactionCard";
 import { transaction } from "mocks/transaction";
+import MainTransactionCard from "module/transaction/component/display/MainTransactionCard/TransactionCard";
 
 describe("TransactionCard tests", () => {
     test("Renders correctly", () => {
-        const screen = render(<TransactionCard {...transaction} />);
+        const screen = render(<MainTransactionCard {...transaction} />);
         expect(screen.getByText("29/01/2022 - 00:00"));
     });
 });
