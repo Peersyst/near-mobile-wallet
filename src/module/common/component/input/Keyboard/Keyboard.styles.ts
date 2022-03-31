313def51800bf9a522d1c814d33a54c1f4a2877d
JordiParraCrespo <jordiparra99@gmail.com>
2022-03-31 12:06:17 +0200

    Keyboard have to be responsive

--- src/module/common/component/input/Keyboard/Keyboard.styles.ts
+++ src/module/common/component/input/Keyboard/Keyboard.styles.ts
@@ -1,6 +1,6 @@
 import styled from "@peersyst/react-native-styled";
 import { Col } from "react-native-components";
 
-export const KeyboardRoot = styled(Col, { gap: "10%", alignItems: "center" })(() => ({
+export const KeyboardRoot = styled(Col, { gap: "12%", alignItems: "center" })(() => ({
     paddingHorizontal: 10,
 }));
