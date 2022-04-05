a39007045d73779ea053ae5921697dce5df2b4f8
JordiParraCrespo <jordiparra99@gmail.com>
2022-04-05 19:02:11 +0200

    linting

--- src/module/dao/mock/daoBalance.ts
+++ src/module/dao/mock/daoBalance.ts
@@ -1,6 +1,6 @@
 import { DAOBalance } from "module/common/service/mock/CkbServiceMock.types";
 
 export const daoBalance: DAOBalance = {
-    daoDeposit: Math.floor(new Date().getSeconds() / 60 * 876),
+    daoDeposit: Math.floor((new Date().getSeconds() / 60) * 876),
     daoCompensation: 2.4,
 };
