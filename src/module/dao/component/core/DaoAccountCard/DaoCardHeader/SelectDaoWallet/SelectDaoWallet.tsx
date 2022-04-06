5df0336d98c5f42605d95e67bdf9ffa758a7b316
Jordi Parra Crespo <jordiparra99@gmail.com>
2022-04-06 16:56:46 +0200

    force rerender on SelectDaoWallet

--- src/module/dao/component/core/DaoAccountCard/DaoCardHeader/SelectDaoWallet/SelectDaoWallet.tsx
+++ src/module/dao/component/core/DaoAccountCard/DaoCardHeader/SelectDaoWallet/SelectDaoWallet.tsx
@@ -2,11 +2,30 @@ import WalletSelector from "module/wallet/component/input/WalletSelector/WalletS
 import { ThemeProvider } from "@peersyst/react-native-styled";
 import { theme } from "module/common/style/theme";
 import { SelectDaoWalletIcon } from "./SelectDaoWallet.styles";
+import { useEffect, useState } from "react";
+import useWalletState from "module/wallet/hook/useWalletState";
 
 const SelectDaoWallet = (): JSX.Element => {
+    const {
+        state: { selectedWallet },
+    } = useWalletState();
+
+    const [rerender, setRerender] = useState<boolean>(false);
+    useEffect(() => {
+        setRerender(true);
+    }, [selectedWallet]);
+    useEffect(() => {
+        if (rerender) setRerender(false);
+    }, [rerender]);
     return (
         <ThemeProvider theme={theme}>
-            <WalletSelector updateSelectedWalletState DisplayComponent={<SelectDaoWalletIcon />} />
+            {!rerender && (
+                <WalletSelector
+                    defaultValue={selectedWallet || undefined}
+                    updateSelectedWalletState
+                    DisplayComponent={<SelectDaoWalletIcon />}
+                />
+            )}
         </ThemeProvider>
     );
 };
