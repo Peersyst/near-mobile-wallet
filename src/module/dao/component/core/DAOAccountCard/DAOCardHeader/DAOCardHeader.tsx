7cbfa6366034171a96bc1b71089e5da900d3f3c5
Jordi Parra Crespo <jordiparra99@gmail.com>
2022-04-06 13:16:59 +0200

    finish merging

--- src/module/dao/component/core/DAOAccountCard/DAOCardHeader/DAOCardHeader.tsx
+++ src/module/dao/component/core/DAOAccountCard/DAOCardHeader/DAOCardHeader.tsx
@@ -1,7 +1,7 @@
 import { InfoIcon } from "icons";
 import { IconButton, Row, Typography } from "react-native-components";
 import { Linking } from "react-native";
-import SelectDaoWallet from "./SelectDaoWallet/SelectDaoWallet";
+import SelectDAOWallet from "./SelectDAOWallet/SelectDAOWallet";
 
 export const DAO_INFO_URL = "https://www.nervos.org/";
 
@@ -14,7 +14,7 @@ const DAOCardHeader = (): JSX.Element => {
             <Typography fontWeight="bold" variant="h2">
                 Nervos DAO
             </Typography>
-            <SelectDaoWallet />
+            <SelectDAOWallet />
         </Row>
     );
 };
--- src/module/dao/component/core/DAOAccountCard/DAOCardHeader/SelectDAOWallet/SelectDAOWallet.tsx
+++ src/module/dao/component/core/DAOAccountCard/DAOCardHeader/SelectDAOWallet/SelectDAOWallet.tsx
@@ -1,13 +1,13 @@
 import WalletSelector from "module/wallet/component/input/WalletSelector/WalletSelector";
 import { ThemeProvider } from "@peersyst/react-native-styled";
 import { theme } from "module/common/style/theme";
-import { SelectDaoWalletIcon } from "./SelectDaoWallet.styles";
+import { SelectDAOWalletIcon } from "./SelectDAOWallet.styles";
 
-const SelectDaoWallet = (): JSX.Element => {
+const SelectDAOWallet = (): JSX.Element => {
     return (
         <ThemeProvider theme={theme}>
-            <WalletSelector updateSelectedWalletState DisplayComponent={<SelectDaoWalletIcon />} />
+            <WalletSelector updateSelectedWalletState DisplayComponent={<SelectDAOWalletIcon />} />
         </ThemeProvider>
     );
 };
-export default SelectDaoWallet;
+export default SelectDAOWallet;
