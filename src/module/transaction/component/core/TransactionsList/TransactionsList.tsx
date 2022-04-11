6440696c5545672d8fbbdaf1723c4844d049befe
AgustinMJ <74896585+AgustinMJ@users.noreply.github.com>
2022-04-11 14:54:21 +0200

    Transactions list isCKBTransactionsFilter

--- src/module/transaction/component/core/TransactionsList/TransactionsList.tsx
+++ src/module/transaction/component/core/TransactionsList/TransactionsList.tsx
@@ -3,14 +3,14 @@ import { translate } from "locale";
 import MainList from "module/main/component/display/MainList/MainList";
 import EmptyListComponent from "module/common/component/display/EmptyListComponent/EmptyListComponent";
 import TransactionCard from "module/transaction/component/display/TransactionCard/TransactionCard";
-import { isDAOTransaction } from "../../utils/isDAOTransaction";
 import { useMemo } from "react";
+import isCKBTransaction from "module/transaction/component/utils/isCKBTransaction";
 
 const TransactionsList = (): JSX.Element => {
     const { data = [], refetch, isLoading } = useGetTransactions();
     //Get the tx that corresponds to ckbs, nfts, and tokens -> not DAO txs
     //Then order them by the latest date
-    const txs = useMemo(() => data.filter((tx) => !isDAOTransaction(tx.type)).reverse(), [data]);
+    const txs = useMemo(() => data.filter((tx) => isCKBTransaction(tx.type)).reverse(), [data]);
     return (
         <MainList
             onRefresh={refetch}
