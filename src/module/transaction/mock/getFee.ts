679c23eaeb7bb09b1495faa5af5720bbb6149ec9
Jordi Parra Crespo <jordiparra99@gmail.com>
2022-04-12 10:20:51 +0200

    gettFee com estava

--- src/module/transaction/mock/getFee.ts
+++ src/module/transaction/mock/getFee.ts
@@ -3,9 +3,9 @@ import { FeeRate, FeeType } from "module/settings/state/SettingsState";
 export default function (feeType: FeeType): Promise<string> {
     return new Promise((resolve) =>
         setTimeout(() => {
-            if (feeType === FeeRate.SLOW) resolve("500");
-            else if (feeType === FeeRate.AVERAGE) resolve("500");
-            else resolve("500");
+            if (feeType === FeeRate.SLOW) resolve("5");
+            else if (feeType === FeeRate.AVERAGE) resolve("10");
+            else resolve("20");
         }, 1000),
     );
 }
