6fdbf658eec2b8652322ba39adfcb1200e1a05b6
AgustinMJ <74896585+AgustinMJ@users.noreply.github.com>
2022-04-26 11:20:54 +0200

    Revert "Update header bg color"

--- src/module/common/component/layout/BasePage/BasePage.tsx
+++ src/module/common/component/layout/BasePage/BasePage.tsx
@@ -4,20 +4,24 @@ import { theme } from "module/common/style/theme";
 import { darkTheme } from "module/common/style/darkTheme";
 import Header from "module/common/component/navigation/Header/Header";
 import { BasePageContent, BasePageRoot } from "./BasePage.styles";
-import { StatusBar } from "expo-status-bar";
+import { StatusBar } from "react-native-components";
+
 const BasePage = ({ children, appearance: appearanceProp, header = true, showIcons }: BasePageProps): JSX.Element => {
     const {
         palette: { mode },
     } = useTheme();
+
     const appearance = appearanceProp || mode;
+
     return (
         <ThemeProvider theme={appearance === "light" ? theme : darkTheme}>
             <BasePageRoot>
-                <ThemeProvider theme={darkTheme}>{header && <Header showIcons={showIcons} />}</ThemeProvider>
+                {header && <Header showIcons={showIcons} />}
                 <BasePageContent header={header}>{children}</BasePageContent>
             </BasePageRoot>
-            <StatusBar style="light" />
+            <StatusBar />
         </ThemeProvider>
     );
 };
+
 export default BasePage;
