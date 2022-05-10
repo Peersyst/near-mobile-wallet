3d7f19a064e2e94608ed59a124e2c8b5058eead3
AgustinMJ <74896585+AgustinMJ@users.noreply.github.com>
2022-05-10 08:27:20 +0200

    status bar automatic color fix

--- src/module/common/component/base/feedback/Backdrop/Backdrop.tsx
+++ src/module/common/component/base/feedback/Backdrop/Backdrop.tsx
@@ -92,6 +92,7 @@ export default function Backdrop({
             propagateSwipe={propagateSwipe}
             style={[{ margin: 0, justifyContent: "center", alignItems: "center" }, style]}
             onResponderStart={() => toastActive && hideToast()}
+            statusBarTranslucent
         >
             {typeof children === "function" ? children(open, setOpen) : children}
             {entered && !toastWasActive && toastActive && open && (
