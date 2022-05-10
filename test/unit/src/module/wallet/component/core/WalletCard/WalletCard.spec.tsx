5d645588fd8c0a103a85196c4bec3aa5f73b4cae
Jordi Parra Crespo <jordiparra99@gmail.com>
2022-05-10 17:34:30 +0200

    test(walletcard): add test to verify that the shows correctly the fiat balance

--- test/unit/src/module/wallet/component/core/WalletCard/WalletCard.spec.tsx
+++ test/unit/src/module/wallet/component/core/WalletCard/WalletCard.spec.tsx
@@ -1,6 +1,6 @@
 import { translate } from "locale";
 import { render } from "test-utils";
-import { waitFor } from "@testing-library/react-native";
+import { fireEvent, waitFor } from "@testing-library/react-native";
 import WalletCard from "module/wallet/component/core/WalletCard/WalletCard";
 import * as UseWalletState from "module/wallet/hook/useWalletState";
 import { mockedUseWallet } from "mocks/useWalletState";
@@ -8,6 +8,9 @@ import { wallet } from "mocks/wallet";
 import { CKBSDKService } from "module/common/service/CkbSdkService";
 import { serviceInstancesMap } from "module/wallet/state/WalletState";
 import { MnemonicMocked } from "mocks/MnemonicMocked";
+import * as useCkbConversion from "module/common/hook/useCkbConversion";
+import * as Recoil from "recoil";
+import { Vibration } from "react-native";
 
 describe("WalletCard tests", () => {
     const sdkInstance = new CKBSDKService(MnemonicMocked);
@@ -16,7 +19,7 @@ describe("WalletCard tests", () => {
         jest.restoreAllMocks();
     });
 
-    test("Renders correctly", async () => {
+    beforeEach(() => {
         jest.spyOn(UseWalletState, "default").mockReturnValue(mockedUseWallet);
         jest.spyOn(serviceInstancesMap, "get").mockReturnValue(sdkInstance);
         jest.spyOn(sdkInstance, "getCKBBalance").mockReturnValue({
@@ -24,8 +27,10 @@ describe("WalletCard tests", () => {
             occupiedBalance: 9600,
             freeBalance: 10400,
         });
-        const screen = render(<WalletCard wallet={wallet} />);
+    });
 
+    test("Renders correctly", async () => {
+        const screen = render(<WalletCard wallet={wallet} />);
         /**Account header */
         expect(screen.getByText(mockedUseWallet.state.wallets[0].name)).toBeDefined();
         expect(screen.getByTestId("EditIcon")).toBeDefined();
@@ -41,5 +46,22 @@ describe("WalletCard tests", () => {
         expect(screen.getByTestId("ReceiveIcon")).toBeDefined();
         expect(screen.getByTestId("SendIcon")).toBeDefined();
         expect(screen.getByText(translate("receive"))).toBeDefined();
+    });
+
+    test("Change the currency when the user clics on the balance", async () => {
+        jest.spyOn(Recoil, "useRecoilValue").mockReturnValue({ fiat: "eur" });
+        jest.spyOn(useCkbConversion, "default").mockReturnValue({ value: 10, convertBalance: jest.fn() });
+        const mockedVibrate = jest.fn();
+        jest.mock("react-native/Libraries/Vibration/Vibration", () => ({
+            vibrate: mockedVibrate,
+        }));
+        const screen = render(<WalletCard wallet={wallet} />);
+        /**Account Balance */
+        await waitFor(() => expect(screen.getByText("10,400")).toBeDefined());
+        const text = screen.getByText("ckb");
+        fireEvent.press(text);
+        await waitFor(() => expect(screen.getByText("10")).toBeDefined());
+        expect(screen.getByText("eur")).toBeDefined();
+        expect(mockedVibrate).toHaveBeenCalled();
     });
 });
