885b4493dfdfe9065c082429a814c07f642d0760
Jordi Parra Crespo <jordiparra99@gmail.com>
2022-05-17 10:44:18 +0200

    fix: token mock

--- src/module/token/mock/token.ts
+++ src/module/token/mock/token.ts
@@ -82,16 +82,7 @@ export const UknownToken: TokenType = {
 };
 
 export const Token: TokenAmount = {
-    type: {
-        name: "Wrapped BTC",
-        description: "ForceBridge from BSC",
-        tokenName: "BTC|bsc",
-        imageUri: "https://bitcoin.org/img/icons/opengraph.png?1644775669",
-        args: "0x9ea7beb4a36469e00bb30dbac75e93672441b483d519556ba9d1424b9294eae5",
-        codeHash: "0x5e7a36a77e68eecc013dfa2fe6a23f3b6c344b04005808694ae6dd45eea4cfd5",
-        hashType: "type",
-        apiId: "wrapped-bitcoin",
-    },
+    type: tokensList[tokensList.length - 1],
     amount: 20000.02,
 };
 
