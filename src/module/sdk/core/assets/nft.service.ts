a8e6a8fa0d17dc82ad4611dfdb68d4b8475be465
Joan Grau <jgrau@peersyst.com>
2022-05-31 11:51:26 +0200

    add compatibility for m-nft

--- src/module/sdk/core/assets/nft.service.ts
+++ src/module/sdk/core/assets/nft.service.ts
@@ -1,17 +1,29 @@
 import { Cell } from "@ckb-lumos/lumos";
 import * as NrcSdk from "@rather-labs/nrc-721-sdk";
 import { Logger } from "../../utils/logger";
-import { ConnectionService } from "../connection.service";
+import { ConnectionService, Environments } from "../connection.service";
 import { ScriptType } from "../transaction.service";
 import { NftScript, NftSdk } from "./nft.types";
 
 export interface Nft {
     tokenId: string;
     tokenUri: string;
-    data: any;
     nftName: string;
-    nftSymbol: string;
-    nftExtraData: string;
+    nftSymbol?: string;
+    data?: any;
+    nftExtraData?: string;
+    issued?: number;
+    total?: number;
+}
+
+export interface MNft {
+    name: string;
+    description: string;
+    renderer: string;
+    version: number;
+    configure: number;
+    issued: number;
+    total: number;
 }
 
 const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
@@ -19,11 +31,42 @@ const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 export class NftService {
     private readonly connection: ConnectionService;
     private readonly logger = new Logger(NftService.name);
+    private readonly mNftCodeHash = "0x2b24f0d644ccbdd77bbf86b27c8cca02efa0ad051e447c212636d9ee7acaaec9";
+    private readonly mNftClassCodeHash = "0xd51e6eaf48124c601f41abe173f1da550b4cbca9c6a166781906a287abbb3d9a";
     private nftSdk: NftSdk = null!;
     private initializing = false;
 
     constructor(connectionService: ConnectionService) {
         this.connection = connectionService;
+    }
+
+    private static mNftFormat(dataHex: string): MNft {
+        const data = dataHex.slice(2);
+        const version = parseInt(data.slice(0, 1), 16);
+        const total = parseInt(data.slice(1, 10), 16);
+        const issued = parseInt(data.slice(10, 18), 16);
+        const configure = parseInt(data.slice(18, 20), 16);
+        const nameSize = parseInt(data.slice(20, 24), 16);
+        const nameEnd = 24 + nameSize * 2;
+        const name = decodeURIComponent(data.slice(24, nameEnd).replace(/[0-9a-f]{2}/g, "%$&"));
+        const descriptionSizeEnd = nameEnd + 4;
+        const descriptionSize = parseInt(data.slice(nameEnd, descriptionSizeEnd), 16);
+        const descriptionEnd = descriptionSizeEnd + descriptionSize * 2;
+        const description = decodeURIComponent(data.slice(descriptionSizeEnd, descriptionEnd).replace(/[0-9a-f]{2}/g, "%$&"));
+        const rendererSizeEnd = descriptionEnd + 4;
+        const rendererSize = parseInt(data.slice(descriptionEnd, rendererSizeEnd), 16);
+        const rendererEnd = rendererSizeEnd + rendererSize * 2;
+        const renderer = decodeURIComponent(data.slice(rendererSizeEnd, rendererEnd).replace(/[0-9a-f]{2}/g, "%$&"));
+
+        return {
+            name: name,
+            description: description,
+            renderer: renderer,
+            version: version,
+            configure: configure,
+            issued: issued,
+            total: total,
+        };
     }
 
     async initialize() {
@@ -68,6 +111,9 @@ export class NftService {
 
     private async getNftFromCell(cell: Cell): Promise<Nft | null> {
         const cellTypeScript = this.cellToNftScript(cell);
+        if (!cellTypeScript) {
+            return null;
+        }
 
         let isNftCell: boolean;
         try {
@@ -76,7 +122,7 @@ export class NftService {
             isNftCell = false;
         }
 
-        if (cellTypeScript && isNftCell) {
+        if (isNftCell) {
             const nft = await this.nftSdk!.nftCell.read(cellTypeScript);
 
             return {
@@ -87,6 +133,39 @@ export class NftService {
                 nftSymbol: nft.factoryData.symbol,
                 nftExtraData: nft.factoryData.extraData,
             };
+        }
+        if (cellTypeScript.codeHash === this.mNftCodeHash && this.connection.getEnvironment() === Environments.Mainnet) {
+            const cellProvider = this.connection.getCellProvider({
+                type: {
+                    code_hash: this.mNftClassCodeHash,
+                    args: cellTypeScript.args.slice(0, -8),
+                    hash_type: "type",
+                },
+            });
+
+            const cells: Cell[] = [];
+            const cellCollector = cellProvider.collector({});
+            for await (const cell of cellCollector.collect()) {
+                cells.push(cell);
+            }
+
+            if (cells.length === 1) {
+                const mNft = NftService.mNftFormat(cells[0].data);
+
+                return {
+                    nftName: mNft.name,
+                    tokenId: parseInt(cellTypeScript.args.slice(-8), 16).toString(),
+                    tokenUri: mNft.renderer,
+                    issued: mNft.issued,
+                    total: mNft.total,
+                    data: {
+                        description: mNft.description,
+                        version: mNft.version,
+                        configure: mNft.configure,
+                        type: "m-NFT",
+                    },
+                };
+            }
         }
 
         return null;
