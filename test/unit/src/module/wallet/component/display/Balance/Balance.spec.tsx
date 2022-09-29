3295f43bcc125be69fe05624cc96f65d9368bc17
JordiParraCrespo <jordiparra99@gmail.com>
2022-09-29 16:00:19 +0200

    fixes balance

--- test/unit/src/module/wallet/component/display/Balance/Balance.spec.tsx
+++ test/unit/src/module/wallet/component/display/Balance/Balance.spec.tsx
@@ -1,10 +1,32 @@
 import Balance from "module/wallet/component/display/Balance/Balance";
+import { getActionLabel } from "module/wallet/component/display/Balance/utils/getActionLabel";
+import { getCurrencyUnit } from "module/wallet/component/display/Balance/utils/getCurrencyUnit";
 import { render } from "test-utils";
 
 describe("Text for the Balance component", () => {
     test("Renders correctly", () => {
         const screen = render(<Balance balance={"100"} variant={"h1"} />);
         expect(screen.getByText("100")).toBeDefined();
+    });
+    test("Renders correctly near", () => {
+        const screen = render(<Balance balance={"100"} variant={"h1"} units="token" />);
+        expect(screen.getByText("100 NEAR")).toBeDefined();
+    });
+    test("Renders correctly eur", () => {
+        const screen = render(<Balance balance={"100"} variant={"h1"} units="eur" />);
+        expect(screen.getByText("100 " + getCurrencyUnit["eur"])).toBeDefined();
+    });
+    test("Renders correctly dollar", () => {
+        const screen = render(<Balance balance={"100"} variant={"h1"} units="usd" />);
+        expect(screen.getByText("100 " + getCurrencyUnit["usd"])).toBeDefined();
+    });
+    test("Renders correctly round", () => {
+        const screen = render(<Balance action="add" balance={"100"} variant={"h1"} units="usd" />);
+        expect(screen.getByText(getActionLabel["add"] + "100 " + getCurrencyUnit["usd"])).toBeDefined();
+    });
+    test("Renders correctly add", () => {
+        const screen = render(<Balance action="round" balance={"100"} variant={"h1"} units="usd" />);
+        expect(screen.getByText(getActionLabel["round"] + "100 " + getCurrencyUnit["usd"])).toBeDefined();
     });
     test("Renders correctly when loading", () => {
         const screen = render(<Balance balance={"100"} variant={"h1"} isLoading />);
