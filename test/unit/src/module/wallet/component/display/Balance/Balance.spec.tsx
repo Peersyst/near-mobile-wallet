0ef8f128658919ac0439e85ea737fef0fe767d56
JordiParraCrespo <jordiparra99@gmail.com>
2022-09-30 16:26:04 +0200

    fixes

--- test/unit/src/module/wallet/component/display/Balance/Balance.spec.tsx
+++ test/unit/src/module/wallet/component/display/Balance/Balance.spec.tsx
@@ -1,5 +1,5 @@
 import Balance from "module/wallet/component/display/Balance/Balance";
-import { getActionLabel } from "module/wallet/component/display/Balance/utils/getActionLabel";
+import { ACTION_LABEL } from "module/wallet/component/display/Balance/utils/getActionLabel";
 import { CURRENCY_UNIT } from "module/wallet/component/display/Balance/utils/currencies";
 import { render } from "test-utils";
 
@@ -17,15 +17,15 @@ describe("Text for the Balance component", () => {
         expect(screen.getByText("100 " + CURRENCY_UNIT["eur"])).toBeDefined();
     });
     test("Renders correctly dollar", () => {
-        const screen = render(<Balance balance={"100"} variant={"h1"} units="usd" />);
-        expect(screen.getByText("100 " + CURRENCY_UNIT["usd"])).toBeDefined();
+        const screen = render(<Balance balance={"100"} variant={"h1"} units="usd" action="add" />);
+        expect(screen.getByText(ACTION_LABEL["add"] + "100 " + CURRENCY_UNIT["usd"])).toBeDefined();
     });
     test("Renders correctly round", () => {
-        const screen = render(<Balance action="add" balance={"100"} variant={"h1"} units="usd" />);
-        expect(screen.getByText(getActionLabel["add"] + "100 " + CURRENCY_UNIT["usd"])).toBeDefined();
-    });
-    test("Renders correctly add", () => {
         const screen = render(<Balance action="round" balance={"100"} variant={"h1"} units="usd" />);
-        expect(screen.getByText(getActionLabel["round"] + "100 " + CURRENCY_UNIT["usd"])).toBeDefined();
+        expect(screen.getByText(ACTION_LABEL["round"] + "100 " + CURRENCY_UNIT["usd"])).toBeDefined();
+    });
+    test("Renders correctly when loading", () => {
+        const screen = render(<Balance balance={"100"} variant={"h1"} isLoading />);
+        expect(screen.getByTestId("ActivityIndicator")).toBeDefined();
     });
 });
