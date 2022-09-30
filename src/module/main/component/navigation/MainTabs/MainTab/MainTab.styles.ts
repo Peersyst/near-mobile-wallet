1bb3620ff01fe1182ee8e85933cdb9bea95be83f
AgustinMJ <74896585+AgustinMJ@users.noreply.github.com>
2022-09-30 17:00:51 +0200

    Improve tabs

--- src/module/main/component/navigation/MainTabs/MainTab/MainTab.styles.ts
+++ src/module/main/component/navigation/MainTabs/MainTab/MainTab.styles.ts
@@ -2,6 +2,6 @@ import styled from "@peersyst/react-native-styled";
 import { Tab } from "@peersyst/react-native-components";
 
 export const MainTabRoot = styled(Tab)(() => ({
-    paddingVertical: 22,
+    paddingVertical: 20,
     marginHorizontal: 8,
 }));
