195d9cf3a61778ff0a34c7e8bf862ed90a63ef80
Joan Grau <jgrau@peersyst.com>
2022-09-30 13:54:33 +0200

    add near-api-js and its functions - working

--- src/module/common/service/NearSdkService.ts
+++ src/module/common/service/NearSdkService.ts
@@ -1,4 +1,4 @@
-// import { connect, keyStores, utils, Near, ConnectConfig, KeyPair } from "near-api-js";
+import { connect, keyStores, utils, Near, ConnectConfig, KeyPair } from "near-api-js";
 import { config } from "config";
 /// const { parseSeedPhrase, generateSeedPhrase } = require("near-seed-phrase");
 
@@ -10,46 +10,48 @@ export enum Chains {
 }
 
 export class NearSDKService {
-    // private connection?: Near;
-    // private nearConfig: ConnectConfig;
+    private connection?: Near;
+    private nearConfig: ConnectConfig;
     private mnemonic: string;
 
     constructor(chain: Chains, nameId: string, mnemonic?: string) {
-        // let keyPair: KeyPair;
+        let keyPair: KeyPair;
 
         if (mnemonic) {
             this.mnemonic = mnemonic;
             // const { secretKey } = parseSeedPhrase(mnemonic);
-            // keyPair = utils.KeyPair.fromString(secretKey);
+            const secretKey = this.mnemonic;
+            keyPair = utils.KeyPair.fromString(secretKey);
         } else {
             // const { seedPhrase, secretKey } = generateSeedPhrase();
             // this.mnemonic = seedPhrase;
-            this.mnemonic = `${config.nearMainnetUrl} ${config.nearTestnetUrl}`;
-            // keyPair = utils.KeyPair.fromString(secretKey);
+            this.mnemonic = "hola";
+            const secretKey = this.mnemonic;
+            keyPair = utils.KeyPair.fromString(secretKey);
         }
 
-        // const keyStore = new keyStores.InMemoryKeyStore();
-        // keyStore.setKey(chain, nameId, keyPair);
+        const keyStore = new keyStores.InMemoryKeyStore();
+        keyStore.setKey(chain, nameId, keyPair);
 
-        // if (chain === Chains.MAINNET) {
-        //     this.nearConfig = {
-        //         networkId: chain,
-        //         keyStore,
-        //         nodeUrl: config.nearMainnetUrl,
-        //     };
-        // } else if (chain === Chains.TESTNET) {
-        //     this.nearConfig = {
-        //         networkId: chain,
-        //         keyStore,
-        //         nodeUrl: config.nearTestnetUrl,
-        //     };
-        // } else {
-        //     throw new Error("Chain not implemented yet");
-        // }
+        if (chain === Chains.MAINNET) {
+            this.nearConfig = {
+                networkId: chain,
+                keyStore,
+                nodeUrl: config.nearMainnetUrl,
+            };
+        } else if (chain === Chains.TESTNET) {
+            this.nearConfig = {
+                networkId: chain,
+                keyStore,
+                nodeUrl: config.nearTestnetUrl,
+            };
+        } else {
+            throw new Error("Chain not implemented yet");
+        }
     }
 
     async connect(): Promise<void> {
-        // this.connection = await connect(this.nearConfig);
+        this.connection = await connect(this.nearConfig);
     }
 
     static async createAndConnect(chain: Chains, nameId: string, mnemonic: string): Promise<NearSDKService> {
