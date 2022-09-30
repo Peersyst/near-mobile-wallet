11af1c6e459bd73f568e8d989d09fee16d8338b2
JordiParraCrespo <jordiparra99@gmail.com>
2022-09-30 16:27:58 +0200

    style: fix linting

--- src/module/home/component/core/HomeSlider.tsx
+++ src/module/home/component/core/HomeSlider.tsx
@@ -1,4 +1,3 @@
-import { PagerView } from "@peersyst/react-native-components";
 import WalletCard from "module/wallet/component/core/WalletCard/WalletCard";
 import AddWalletCard from "module/wallet/component/display/AddWalletCard/AddWalletCard";
 import useWalletState from "module/wallet/hook/useWalletState";
