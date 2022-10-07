ab628b3147a3601141adfabfc26a84758e39dbd0
GuillemGarciaDev <guillem.garcia.gil@protonmail.com>
2022-10-07 13:38:15 +0200

    formatDate w/ new tests

--- test/unit/src/utils/formatDate.spec.ts
+++ test/unit/src/utils/formatDate.spec.ts
@@ -1,10 +1,16 @@
 import formatDate from "utils/formatDate";
 
 describe("Format date tests", () => {
-    test("Returns 04/01/1970", () => {
-        expect(formatDate(new Date(1970, 0, 4))).toEqual("04/01/1970 - 00:00");
+    test("Returns 04/01/1970 in date-hours mode", () => {
+        expect(formatDate(new Date(1970, 0, 4), "date-hours")).toEqual("04/01/1970 - 00:00");
     });
-    test("Returns 14/11/1970", () => {
-        expect(formatDate(new Date(1970, 10, 14))).toEqual("14/11/1970 - 00:00");
+    test("Returns 04/01/1970 in weekday mode", () => {
+        expect(formatDate(new Date(1970, 0, 4), "weekday")).toEqual("Sun 04 Jan, 1970");
+    });
+    test("Returns 14/11/1970 in date-hours mode", () => {
+        expect(formatDate(new Date(1970, 10, 14), "date-hours")).toEqual("14/11/1970 - 00:00");
+    });
+    test("Returns 14/11/1970 in weekday mode", () => {
+        expect(formatDate(new Date(1970, 10, 14), "weekday")).toEqual("14/11/1970 - 00:00");
     });
 });
