bf1ef36125c103dc29b787dedeaf95146ae6d331
Joan Grau <jgrau@peersyst.com>
2022-10-19 16:54:52 +0200

    add stake, unstake, withdraw, balance and validators

---
+++ src/module/common/service/near.constants.ts
@@ -0,0 +1,11 @@
+export const MINIMUM_UNSTAKED = 100;
+export const DEPOSIT_STAKE_METHOD = "deposit_and_stake";
+export const UNSTAKE_METHOD = "unstake";
+export const UNSTAKE_ALL_METHOD = "unstake_all";
+export const WITHDRAW_METHOD = "withdraw";
+export const WITHDRAW_ALL_METHOD = "withdraw_all";
+export const ACCOUNT_TOTAL_BALANCE_METHOD = "get_account_total_balance";
+export const ACCOUNT_STAKED_BALANCE_METHOD = "get_account_staked_balance";
+export const ACCOUNT_UNSTAKED_BALANCE_METHOD = "get_account_unstaked_balance";
+export const IS_ACCOUNT_UNSTAKED_BALANCE_AVAILABLE_METHOD = "is_account_unstaked_balance_available";
+export const REWARD_FEE_FRACTION_METHOD = "get_reward_fee_fraction";
