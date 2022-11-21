f4fa2cba64d5e6b000cc1c24ddc4d67aa6f7f7b8
JordiParraCrespo <jordiparra99@gmail.com>
2022-11-21 13:00:26 +0100

    fix warings

--- App.tsx
+++ App.tsx
@@ -1,14 +1,15 @@
 import Root from "./src";
 import useCachedResources from "./src/module/common/hook/useCachedResources";
+import { LogBox } from "react-native";
+import React from "react";
 // Polyfill Intl as it is not included in RN
 import "intl";
 
-//Explanation link :https://github.com/facebook/react-native/issues/12981#issuecomment-652745831
-import { LogBox } from "react-native";
-
 const App = (): JSX.Element | null => {
     const isLoadingComplete = useCachedResources();
-    LogBox.ignoreLogs(["Setting a timer"]);
+    LogBox.ignoreLogs(["Setting a timer"]); // https://github.com/facebook/react-native/issues/12981#issuecomment-652745831
+    LogBox.ignoreLogs(["Require cycle:"]); // Consider refactoring to remove the need for a cycle
+    LogBox.ignoreLogs(["new NativeEventEmitter"]); // Warning from expo-clipboard (React 18)
     return isLoadingComplete ? <Root /> : null;
 };
 
