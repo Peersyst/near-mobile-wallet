986033e9d4933086d684cb8e9cfc91d91fa992c4
JordiParraCrespo <jordiparra99@gmail.com>
2022-11-23 11:58:27 +0100

    fix locale

--- src/locale/i18n.ts
+++ src/locale/i18n.ts
@@ -2,7 +2,10 @@ import i18next from "i18next";
 import { initReactI18next } from "react-i18next";
 import { en } from "./locales/en/en";
 import { es } from "./locales/es/es";
-
+// Polyfill Intl as it is not included in RN
+import "intl";
+import "intl/locale-data/jsonp/en";
+import "intl/locale-data/jsonp/es";
 import LanguageDetectorPlugin from "./pluguins/LanguageDetectorPlugin/LanguageDetectorPlugin";
 
 export const defaultNS = "translation";
