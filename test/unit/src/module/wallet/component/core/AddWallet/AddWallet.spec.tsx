512cc78da3bda9f367d9014fdb9822448bc3ae37
JordiParraCrespo <jordiparra99@gmail.com>
2022-12-05 09:09:23 +0100

    feat: add a wallet test

--- test/unit/src/module/wallet/component/core/AddWallet/AddWallet.spec.tsx
+++ test/unit/src/module/wallet/component/core/AddWallet/AddWallet.spec.tsx
@@ -4,5 +4,6 @@ import AddWallet from "module/wallet/component/core/AddWallet/AddWallet";
 describe("AddWallet tests", () => {
     test("Renders correctly", () => {
         render(<AddWallet />);
+        expect(screen.getByText(translate("add_a_wallet_txt")));
     });
 });
