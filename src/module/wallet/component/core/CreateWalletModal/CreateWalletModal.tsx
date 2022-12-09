73a74a8c467f718ba732a2f5a231512b8c8edc55
JordiParraCrespo <jordiparra99@gmail.com>
2022-12-09 11:21:13 +0100

    fix linting

--- src/module/wallet/component/core/CreateWalletModal/CreateWalletModal.tsx
+++ src/module/wallet/component/core/CreateWalletModal/CreateWalletModal.tsx
@@ -1,5 +1,4 @@
 import { createModal, ExposedBackdropProps, TabPanel, Tabs } from "@peersyst/react-native-components";
-import SetWalletNameScreen from "module/wallet/screen/SetWalletNameScreen";
 import WalletAdvisesScreen from "module/wallet/screen/WalletAdvisesScreen";
 import WalletMnemonicScreen from "module/wallet/screen/WalletMnemonicScreen";
 import PickWalletMnemonicScreen from "module/wallet/screen/PickWalletMnemonicScreen/PickWalletMnemonicScreen";
