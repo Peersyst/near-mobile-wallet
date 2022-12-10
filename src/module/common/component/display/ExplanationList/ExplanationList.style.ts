1ef43294f9e1290328614993d06797093fec8269
JordiParraCrespo <jordiparra99@gmail.com>
2022-12-10 16:24:06 +0100

    fix

--- src/module/common/component/display/ExplanationList/ExplanationList.style.ts
+++ src/module/common/component/display/ExplanationList/ExplanationList.style.ts
@@ -3,10 +3,8 @@ import styled from "@peersyst/react-native-styled";
 
 export const ValidIcon = styled(Icon)(({ theme }) => ({
     color: theme.palette.status.success,
-    fontSize: 24,
 }));
 
 export const InvalidIcon = styled(Icon)(({ theme }) => ({
     color: theme.palette.status.error,
-    fontSize: 24,
 }));
