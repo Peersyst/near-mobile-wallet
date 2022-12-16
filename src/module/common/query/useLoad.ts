9284cea58a3066ef169518cd13e32b5d1b4f7dcc
JordiParraCrespo <jordiparra99@gmail.com>
2022-12-16 10:59:51 +0100

    fix useLoad

--- src/module/common/query/useLoad.ts
+++ src/module/common/query/useLoad.ts
@@ -14,15 +14,8 @@ export function useLoad(): boolean {
         const getStorage = async () => {
             const settings = (await SettingsStorage.getAllSettings()) || defaultSettingsState;
             let hasPreviousWallet = await recoverWallets(settings.network);
-            if (hasPreviousWallet) setSettingsState(settings);
-            else {
-                const network: NetworkType = settings.network === Chains.TESTNET ? Chains.MAINNET : Chains.TESTNET;
-                hasPreviousWallet = await recoverWallets(network);
-
-                if (hasPreviousWallet) {
-                    await SettingsStorage.set({ network });
-                    setSettingsState({ ...settings, network });
-                }
+            if (hasPreviousWallet) {
+                setSettingsState(settings);
             }
             setLoading(false);
         };
