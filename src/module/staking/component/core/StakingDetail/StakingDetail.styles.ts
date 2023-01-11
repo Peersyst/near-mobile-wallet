b637a5978658334a99ae2fb7d703aee740ff6644
GuillemGarciaDev <guillem.garcia.gil@protonmail.com>
2023-01-11 14:16:50 +0100

    IconCircleWrapper general

--- src/module/staking/component/core/StakingDetail/StakingDetail.styles.ts
+++ src/module/staking/component/core/StakingDetail/StakingDetail.styles.ts
@@ -1,23 +1,12 @@
 import styled from "@peersyst/react-native-styled";
-import { Paper, Row } from "@peersyst/react-native-components";
+import { Row } from "@peersyst/react-native-components";
 import { NearIcon } from "icons";
-import { alpha } from "@peersyst/react-utils";
 
 export const StakingDetailRoot = styled(Row)(() => ({
     paddingVertical: 16,
     paddingHorizontal: 20,
     minHeight: 90,
     maxHeight: 90,
-}));
-
-export const IconCircleWrapper = styled(Paper, { elevation: 0 })(({ theme }) => ({
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    width: 44,
-    height: 44,
-    borderRadius: 100,
-    backgroundColor: alpha(theme.palette.primary, 0.12),
 }));
 
 export const CircleNearIcon = styled(NearIcon)(({ theme }) => ({
--- src/module/staking/component/core/StakingDetail/StakingDetail.tsx
+++ src/module/staking/component/core/StakingDetail/StakingDetail.tsx
@@ -2,10 +2,13 @@ import { Col, Row, useModal } from "@peersyst/react-native-components";
 import Typography from "module/common/component/display/Typography/Typography";
 import Button from "module/common/component/input/Button/Button";
 import { useTranslate } from "module/common/hook/useTranslate";
-import { CircleNearIcon, IconCircleWrapper, StakingDetailRoot } from "module/staking/component/core/StakingDetail/StakingDetail.styles";
+import { CircleNearIcon, StakingDetailRoot } from "module/staking/component/core/StakingDetail/StakingDetail.styles";
 import Balance from "module/wallet/component/display/Balance/Balance";
 import UnstakeModal from "module/staking/component/core/UnstakeModal/UnstakeModal";
 import config from "config/config";
+import { alpha } from "@peersyst/react-utils";
+import { IconCircleWrapper } from "module/common/component/display/IconCircleWrapper/IconCircleWrapper.styles";
+import { useTheme } from "@peersyst/react-native-styled";
 
 export interface StakingDetailProps {
     title: string;
@@ -16,11 +19,12 @@ export interface StakingDetailProps {
 const StakingDetail = ({ title, amount, stakeable }: StakingDetailProps): JSX.Element => {
     const translate = useTranslate();
     const { showModal } = useModal();
+    const theme = useTheme();
 
     return (
         <StakingDetailRoot flex={1} gap={12} justifyContent="space-between" alignItems="center">
             <Row gap={12} alignItems="center">
-                <IconCircleWrapper>
+                <IconCircleWrapper size={44} backgroundColor={alpha(theme.palette.primary, 0.12)}>
                     <CircleNearIcon />
                 </IconCircleWrapper>
                 <Col>
