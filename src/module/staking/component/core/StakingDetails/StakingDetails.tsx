61f88e99971e3b87c9f9da59d55bc304baceef84
GuillemGarciaDev <guillem.garcia.gil@protonmail.com>
2023-01-11 13:57:17 +0100

    useGetStakingDetailsSections

--- src/module/staking/component/core/StakingDetails/StakingDetails.tsx
+++ src/module/staking/component/core/StakingDetails/StakingDetails.tsx
@@ -1,46 +1,16 @@
-import StakingDetail, { StakingDetailProps } from "module/staking/component/core/StakingDetail/StakingDetail";
+import StakingDetail from "module/staking/component/core/StakingDetail/StakingDetail";
 import { StakingDetailsRoot } from "module/staking/component/core/StakingDetails/StakingDetails.styles";
-import useGetTotalStaking from "module/staking/query/useGetTotalStaking";
-import useWalletState from "module/wallet/hook/useWalletState";
-import { useTranslate } from "module/common/hook/useTranslate";
 import { Col, Spinner, Suspense } from "@peersyst/react-native-components";
-
-type StakingDetailsSection = Omit<StakingDetailProps, "loading">;
+import useGetStakingDetailsSections from "module/staking/hook/useGetStakingDetailsSections";
 
 const StakingDetails = (): JSX.Element => {
-    const translate = useTranslate();
-
-    const {
-        state: { selectedWallet },
-    } = useWalletState();
-    const { isLoading, data: { staked, rewardsEarned, pending, available } = { staked: 0, pending: 0, available: 0 } } =
-        useGetTotalStaking(selectedWallet);
-
-    const stakingDetailsSections: StakingDetailsSection[] = [
-        {
-            title: translate("totalAmountStaked"),
-            amount: staked,
-            stakeable: true,
-        },
-        {
-            title: translate("rewardsEarned"),
-            amount: rewardsEarned,
-        },
-        {
-            title: translate("pendingRelease"),
-            amount: pending,
-        },
-        {
-            title: translate("availableForWithdrawal"),
-            amount: available,
-        },
-    ];
+    const { isLoading, sections } = useGetStakingDetailsSections();
 
     return (
         <StakingDetailsRoot>
             <Suspense isLoading={isLoading} fallback={<Spinner />}>
                 <Col flex={1}>
-                    {stakingDetailsSections.map(
+                    {sections.map(
                         ({ title, amount, stakeable }, index) =>
                             amount !== undefined && <StakingDetail key={index} title={title} amount={amount} stakeable={stakeable} />,
                     )}
