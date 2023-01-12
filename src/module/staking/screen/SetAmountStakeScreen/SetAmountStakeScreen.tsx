e71b73b1518ea0925e0b929385215cabb5e9ef3e
pablo <silvap.javier@gmail.com>
2023-01-12 12:14:54 +0100

    Add amount price around and fix max function

--- src/module/staking/screen/SetAmountStakeScreen/SetAmountStakeScreen.tsx
+++ src/module/staking/screen/SetAmountStakeScreen/SetAmountStakeScreen.tsx
@@ -2,7 +2,7 @@ import { Col, Form, useSetTab } from "@peersyst/react-native-components";
 import Button from "module/common/component/input/Button/Button";
 import stakeRecoilState from "module/staking/state/StakeState";
 import { SendScreens } from "module/transaction/component/core/SendModal/SendModal";
-import { useRecoilState } from "recoil";
+import { useRecoilState, useRecoilValue } from "recoil";
 import { useTranslate } from "module/common/hook/useTranslate";
 import NEARAmountInput from "module/transaction/component/input/AssetAmountInput/NEARAmountInput/NEARAmountInput";
 import useSelectedWallet from "module/wallet/hook/useSelectedWallet";
@@ -10,6 +10,9 @@ import useGetBalance from "module/wallet/query/useGetBalance";
 import { useFormatBalance } from "module/wallet/component/display/Balance/hook/useFormatBalance";
 import { useState } from "react";
 import { config } from "config";
+import useNativeTokenConversion from "module/common/hook/useNativeTokenConversion";
+import { subtractNearAmounts } from "near-peersyst-sdk";
+import settingsState from "module/settings/state/SettingsState";
 
 export interface SendForm {
     amount: string;
@@ -22,13 +25,24 @@ const SetAmountStakeScreen = () => {
     const { index } = useSelectedWallet();
     const { data: { available } = { available: "0" } } = useGetBalance(index);
     const setTab = useSetTab();
+    const { fiat } = useRecoilValue(settingsState);
 
     const handleSubmit = ({ amount }: SendForm) => {
         setStakeState((oldState) => ({ ...oldState, amount: amount }));
         setTab(SendScreens.AMOUNT_AND_MESSAGE);
     };
 
-    const maxBalance = useFormatBalance(Number(available) - Number(config.estimatedFee), {
+    const maxBalance = subtractNearAmounts(available, config.estimatedFee);
+
+    const maxBalanceInFiat = useNativeTokenConversion(maxBalance);
+
+    const formattedBalanceInFiat = useFormatBalance(maxBalanceInFiat.value, {
+        numberFormatOptions: { maximumFractionDigits: 2 },
+        units: fiat,
+        action: "round",
+    });
+
+    const formattedBalance = useFormatBalance(maxBalance, {
         numberFormatOptions: { maximumFractionDigits: 2 },
     });
 
@@ -43,7 +57,7 @@ const SetAmountStakeScreen = () => {
                 <Col gap={24}>
                     <NEARAmountInput
                         label={translate("enter_amount_want_to_stake")!}
-                        hint={translate("available_balance", { amount: maxBalance, amount_price: 1 })!}
+                        hint={translate("available_balance", { amount: formattedBalance, amount_price: formattedBalanceInFiat })!}
                         index={index}
                         name="amount"
                         suffix={
