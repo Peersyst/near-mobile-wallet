86bfbfd42d17282dfbdd7e02641ac78246df9847
JordiParraCrespo <jordiparra99@gmail.com>
2023-01-16 10:38:33 +0100

    fix test

--- src/module/staking/component/core/UnstakeModal/UnstakeModal.tsx
+++ src/module/staking/component/core/UnstakeModal/UnstakeModal.tsx
@@ -1,8 +1,7 @@
 import { createModal } from "@peersyst/react-native-components";
-import { StakeModalRoot } from "module/staking/component/core/StakeModal/StakeModal.styles";
 
 const UnstakeModal = createModal((): JSX.Element => {
-    return <StakeModalRoot></StakeModalRoot>;
+    return <></>;
 });
 
 export default UnstakeModal;
