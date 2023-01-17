39c076701d3b57bf3d5f12dedd0b382b611ba21c
GuillemGarciaDev <guillem.garcia.gil@protonmail.com>
2023-01-17 13:32:04 +0100

    OnlySingleWallet added to useGetBalance + useGetTotalStaking

--- src/module/staking/query/useGetTotalStaking.ts
+++ src/module/staking/query/useGetTotalStaking.ts
@@ -3,16 +3,19 @@ import { useQuery } from "react-query";
 import { StakingBalance } from "module/sdk";
 import { QueryResult } from "query-utils";
 import Queries from "../../../query/queries";
+import useSameWallet from "module/wallet/hook/useSameWallet";
 
-export default function (index?: number): QueryResult<StakingBalance> {
+export default function (index?: number, onlySingleWallet?: boolean): QueryResult<StakingBalance> {
+    const { sameWallet } = useSameWallet();
     const { index: usedIndex, network, serviceInstance } = useServiceInstance(index);
+    const queryEnabled = !!serviceInstance && (onlySingleWallet ? sameWallet(index) : true);
     return useQuery(
         [Queries.TOTAL_STAKING_BALANCE, usedIndex, network],
         async (): Promise<StakingBalance> => {
             return await serviceInstance.getTotalStakingBalance();
         },
         {
-            enabled: !!serviceInstance,
+            enabled: queryEnabled,
         },
     );
 }
--- src/module/wallet/query/useGetBalance.ts
+++ src/module/wallet/query/useGetBalance.ts
@@ -1,12 +1,15 @@
 import Queries from "../../../query/queries";
 import { useQuery } from "react-query";
 import useServiceInstance from "../hook/useServiceInstance";
+import useSameWallet from "module/wallet/hook/useSameWallet";
 
-const useGetBalance = (index?: number) => {
-    const { serviceInstance, index: usedIndex, network } = useServiceInstance(index);
-    return useQuery([Queries.GET_BALANCE, usedIndex, network], () => serviceInstance.getAccountBalance(), {
+const useGetBalance = (index?: number, onlySingleWallet?: boolean) => {
+    const { sameWallet } = useSameWallet();
+    const { index: usedIndex, network, serviceInstance } = useServiceInstance(index);
+    const queryEnabled = !!serviceInstance && (onlySingleWallet ? sameWallet(index) : true);
+    return useQuery([Queries.GET_BALANCE, usedIndex, network], async () => await serviceInstance.getAccountBalance(), {
         refetchInterval: 1500,
-        enabled: !!serviceInstance,
+        enabled: queryEnabled,
     });
 };
 
