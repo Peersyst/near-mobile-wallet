208fbba82d9d5628ffd3d194a0f2167b539ba482
pablo <silvap.javier@gmail.com>
2023-01-17 14:34:15 +0100

    Fix test

--- test/unit/src/module/transaction/component/input/NEARAmountWithMaxTextField.spec.tsx
+++ test/unit/src/module/transaction/component/input/NEARAmountWithMaxTextField.spec.tsx
@@ -6,13 +6,13 @@ describe("NEARAmountWithMaxTextField Test", () => {
     test("Renders and validates correctly", async () => {
         const balance = new AccountBalanceMock({ available: "10" });
         new UseGetBalanceMock({ balance });
-        render(<NEARAmountWithMaxTextField index={0} placeholder="Enter amount" available="8" />);
+        render(<NEARAmountWithMaxTextField index={0} placeholder="Enter amount" value="8" />);
         //Spinner until the balance is loaded
         expect(screen.getByTestId("ActivityIndicator")).toBeDefined();
         //Wait untill the balance is loaded
         await waitFor(() => expect(screen.getByText("Max")).toBeDefined());
         const maxButton = screen.getByText("Max");
         fireEvent.press(maxButton);
-        expect(screen.getByText("7.9995")).toBeDefined();
+        await waitFor(() => expect(screen.queryByDisplayValue("9.99995")).toBeDefined());
     });
 });
