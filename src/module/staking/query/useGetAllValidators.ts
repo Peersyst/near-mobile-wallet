0506ba7d325ae383f6c8e705f748a87c3758a064
JordiParraCrespo <jordiparra99@gmail.com>
2023-01-18 15:39:07 +0100

    litlle improvement

--- src/module/staking/query/useGetAllValidators.ts
+++ src/module/staking/query/useGetAllValidators.ts
@@ -4,10 +4,10 @@ import Queries from "../../../query/queries";
 import useServiceInstance from "module/wallet/hook/useServiceInstance";
 import { Validator } from "module/sdk";
 
-export default function (index?: number): QueryResult<Validator[]> {
-    const { index: usedIndex, network, serviceInstance, queryEnabled } = useServiceInstance(index);
+export default function (): QueryResult<Validator[]> {
+    const { network, serviceInstance, queryEnabled } = useServiceInstance(0);
     return useQuery(
-        [Queries.GET_ALL_VALIDATOR_IDS, usedIndex, network],
+        [Queries.GET_ALL_VALIDATOR_IDS, network],
         async (): Promise<Validator[]> => {
             return await serviceInstance.getAllValidators();
         },
