da0ec87547347c5931f9fa471a3162e1c5de85f8
GuillemGarciaDev <guillem.garcia.gil@protonmail.com>
2023-01-18 14:21:21 +0100

    Adding StakingValidator mock

--- test/unit/src/module/staking/screen/SelectValidatorScreen/SelectValidatorScreen.spec.tsx
+++ test/unit/src/module/staking/screen/SelectValidatorScreen/SelectValidatorScreen.spec.tsx
@@ -1,9 +1,13 @@
 import { render } from "test-utils";
 import SelectValidatorScreen from "module/staking/screen/SelectValidatorScreen/SelectValidatorScreen";
+import { StakingValidatorMock } from "mocks/common/staking/stakingValidator.mock";
 
 describe("Tests for SelectValidatorScreen", () => {
     test("Renders correctly", () => {
         const mockMessage = "message";
-        render(<SelectValidatorScreen message={} validators={} loading={} onFinish={} />);
+        const mockOnFinish = jest.fn();
+        const mockValidators = [new StakingValidatorMock()];
+
+        render(<SelectValidatorScreen message={mockMessage} validators={mockValidators} loading={false} onFinish={mockOnFinish} />);
     });
 });
