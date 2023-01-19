6d9b93b43a4c9bcd1e5cef66a8c4f78c85007b7b
GuillemGarciaDev <guillem.garcia.gil@protonmail.com>
2023-01-19 17:10:52 +0100

    testing updated

---
+++ test/unit/src/module/staking/screen/BaseSetAmountStakeScreen/AddStakeSetAmountScreen/AddStakeSetAmountScreen.spec.tsx
@@ -0,0 +1,48 @@
+import { config } from "config";
+import { AddStakeScreens } from "module/staking/component/core/AddStakeModal/AddStakeModal";
+import { ACTION_LABEL } from "module/wallet/component/display/Balance/utils/actionLabels";
+import { CURRENCY_UNIT } from "module/wallet/component/display/Balance/utils/currencies";
+import {
+    AccountBalanceMock,
+    UseGetBalanceMock,
+    UseNativeTokenConversionMock,
+    UseServiceInstanceMock,
+    UseSetTabMock,
+    UseWalletStateMock,
+} from "test-mocks";
+import { fireEvent, render, screen, translate, waitFor } from "test-utils";
+import AddStakeSetAmountScreen from "module/staking/screen/BaseSetAmountStakeScreen/AddStakeSetAmountScreen/AddStakeSetAmountScreen";
+
+describe("Test for SetAmountStakeScreen component", () => {
+    test("Renders correctly", async () => {
+        new UseServiceInstanceMock();
+        new UseWalletStateMock();
+        new UseNativeTokenConversionMock({ value: "3000" });
+        const balance = new AccountBalanceMock({ available: "1500.55" });
+        new UseGetBalanceMock({ balance });
+        render(<AddStakeSetAmountScreen />);
+
+        const available = await screen.findByText(
+            translate("available_balance", {
+                amount: "1,500.55 " + config.tokenName,
+                amount_price: ACTION_LABEL["round"] + " 3,000 " + CURRENCY_UNIT["usd"],
+            }),
+        );
+        expect(available).toBeDefined();
+    });
+
+    test("Navigates to next screen", async () => {
+        const { setTab } = new UseSetTabMock();
+        new UseServiceInstanceMock();
+        new UseWalletStateMock();
+        new UseNativeTokenConversionMock({ value: "3000" });
+        new UseGetBalanceMock();
+        render(<AddStakeSetAmountScreen />);
+        const input = screen.getByPlaceholderText(translate("enter_amount"));
+        const btn = screen.getByText(translate("next"));
+        await screen.findByText("Max");
+        fireEvent.changeText(input, "100");
+        fireEvent.press(btn);
+        await waitFor(() => expect(setTab).toHaveBeenCalledWith(AddStakeScreens.SELECT_VALIDATOR));
+    });
+});
--- test/unit/src/module/staking/screen/BaseSetAmountStakeScreen/UnstakeSetAmountScreen/UnstakeSetAmountScreen.spec.tsx
+++ test/unit/src/module/staking/screen/BaseSetAmountStakeScreen/UnstakeSetAmountScreen/UnstakeSetAmountScreen.spec.tsx
@@ -1,17 +1,12 @@
+import { UseGetBalanceMock, UseNativeTokenConversionMock, UseServiceInstanceMock, UseWalletStateMock } from "mocks/common";
+import { AccountBalanceMock } from "mocks/NearSdk";
+import { fireEvent, render, screen, translate, waitFor } from "test-utils";
 import { config } from "config";
-import { AddStakeScreens } from "module/staking/component/core/AddStakeModal/AddStakeModal";
-import SetAmountStakeScreen from "module/staking/screen/BaseSetAmountStakeScreen/AddStakeSetAmountScreen/SetAmountStakeScreen";
 import { ACTION_LABEL } from "module/wallet/component/display/Balance/utils/actionLabels";
 import { CURRENCY_UNIT } from "module/wallet/component/display/Balance/utils/currencies";
-import {
-    AccountBalanceMock,
-    UseGetBalanceMock,
-    UseNativeTokenConversionMock,
-    UseServiceInstanceMock,
-    UseSetTabMock,
-    UseWalletStateMock,
-} from "test-mocks";
-import { fireEvent, render, screen, translate, waitFor } from "test-utils";
+import { UseSetTabMock } from "mocks/genesys";
+import UnstakeSetAmountScreen from "module/staking/screen/BaseSetAmountStakeScreen/UnstakeSetAmountScreen/UnstakeSetAmountScreen";
+import { UnstakeModalScreens } from "module/staking/component/core/UnstakeModal/UnstakeModal";
 
 describe("Test for SetAmountStakeScreen component", () => {
     test("Renders correctly", async () => {
@@ -20,7 +15,8 @@ describe("Test for SetAmountStakeScreen component", () => {
         new UseNativeTokenConversionMock({ value: "3000" });
         const balance = new AccountBalanceMock({ available: "1500.55" });
         new UseGetBalanceMock({ balance });
-        render(<SetAmountStakeScreen />);
+
+        render(<UnstakeSetAmountScreen />);
 
         const available = await screen.findByText(
             translate("available_balance", {
@@ -37,12 +33,14 @@ describe("Test for SetAmountStakeScreen component", () => {
         new UseWalletStateMock();
         new UseNativeTokenConversionMock({ value: "3000" });
         new UseGetBalanceMock();
-        render(<SetAmountStakeScreen />);
+
+        render(<UnstakeSetAmountScreen />);
+
         const input = screen.getByPlaceholderText(translate("enter_amount"));
         const btn = screen.getByText(translate("next"));
         await screen.findByText("Max");
         fireEvent.changeText(input, "100");
         fireEvent.press(btn);
-        await waitFor(() => expect(setTab).toHaveBeenCalledWith(AddStakeScreens.SELECT_VALIDATOR));
+        await waitFor(() => expect(setTab).toHaveBeenCalledWith(UnstakeModalScreens.CONFIRM));
     });
 });
