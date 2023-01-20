0a5ac173033a7a1cebf7be1ae438af6b2a42dc1a
JordiParraCrespo <jordiparra99@gmail.com>
2023-01-20 08:39:10 +0100

    More improvements on news

--- src/module/news/component/display/SimpleNewsCard/SimpleNewsCard.tsx
+++ src/module/news/component/display/SimpleNewsCard/SimpleNewsCard.tsx
@@ -4,17 +4,22 @@ import { Col, WithSkeleton, withSkeleton } from "@peersyst/react-native-componen
 import { formatNews } from "module/news/utils/formatNews";
 import { NewsDto } from "module/news/types";
 import useFormatDate from "module/common/hook/useFormatDate";
-import { placeholder_image } from "images";
+import { near_bg_0, near_bg_1 } from "images";
+import { useRef } from "react";
+
+export const NEWS_BG_IMAGES = [near_bg_0, near_bg_1];
 
 const SimpleNewsCard = ({ ...news }: WithSkeleton<NewsDto>): JSX.Element => {
     const { uri, title, imageUri, date } = formatNews(news);
     const formatDate = useFormatDate();
 
+    const randomImage = useRef(NEWS_BG_IMAGES[Math.floor(Math.random() * NEWS_BG_IMAGES.length)]).current;
+
     return (
         <TouchableWithoutFeedback onPress={() => Linking.openURL(uri)}>
             <SimpleNewsCardRoot elevation={0}>
                 <Col gap={12} justifyContent="center">
-                    <NewsImage source={imageUri !== "" ? { uri: imageUri } : placeholder_image} />
+                    <NewsImage source={imageUri !== "" ? { uri: imageUri } : randomImage} />
                     <Col gap={2}>
                         <NewDate variant="body3Regular">{formatDate(date)}</NewDate>
                         <NewTitle variant="body3Strong">{title}</NewTitle>
