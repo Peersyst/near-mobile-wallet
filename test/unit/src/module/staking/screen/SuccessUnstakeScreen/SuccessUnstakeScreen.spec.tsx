89a955634276a5327be8ae8cb9a999b5b9f02b3e
GuillemGarciaDev <guillem.garcia.gil@protonmail.com>
2023-01-20 09:51:17 +0100

    SuccessUnstakeScreen test fix

--- test/unit/src/module/staking/screen/SuccessUnstakeScreen/SuccessUnstakeScreen.spec.tsx
+++ test/unit/src/module/staking/screen/SuccessUnstakeScreen/SuccessUnstakeScreen.spec.tsx
@@ -1,17 +1,18 @@
 import SuccessUnstakeScreen from "module/staking/screen/SuccessUnstakeScreen/SuccessUnstakeScreen";
-import { StakeStateMock } from "test-mocks";
+import { StakeStateMock, UseModalMock } from "test-mocks";
 
 import { fireEvent, render, screen, translate } from "test-utils";
+import UnstakeModal from "module/staking/component/core/UnstakeModal/UnstakeModal";
 
 describe("SuccessUnstakeScreen", () => {
     test("renders correctly", () => {
-        const mockOnClose = jest.fn();
+        const { hideModal } = new UseModalMock();
         new StakeStateMock();
         render(<SuccessUnstakeScreen />);
         expect(screen.getByText(translate("unstake_success"))).toBeDefined();
         expect(screen.getByText(translate("unstake_withdraw_waiting_time"))).toBeDefined();
         const btn = screen.getByRole("button", { name: translate("close") });
         fireEvent.press(btn);
-        expect(mockOnClose).toBeCalledTimes(1);
+        expect(hideModal).toHaveBeenCalledWith(UnstakeModal.id);
     });
 });
