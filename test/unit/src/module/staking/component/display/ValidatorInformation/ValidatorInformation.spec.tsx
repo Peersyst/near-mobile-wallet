1f47ed0dead8626b3b89d175d83f36d26e8746d6
GuillemGarciaDev <guillem.garcia.gil@protonmail.com>
2023-01-20 08:07:40 +0100

    ValidatorInformation test update

--- test/unit/src/module/staking/component/display/ValidatorInformation/ValidatorInformation.spec.tsx
+++ test/unit/src/module/staking/component/display/ValidatorInformation/ValidatorInformation.spec.tsx
@@ -2,6 +2,7 @@ import { render, translate, screen } from "test-utils";
 import { capitalize } from "@peersyst/react-utils";
 import { ValidatorMock } from "mocks/NearSdk/validator.mock";
 import ValidatorInformation from "module/staking/component/display/ValidatorInformation/ValidatorInformation";
+import { fireEvent } from "@testing-library/react-native";
 
 describe("Tests for ValidatorInformation", () => {
     test("Renders correctly with active validator", () => {
@@ -12,5 +13,21 @@ describe("Tests for ValidatorInformation", () => {
         expect(screen.getByText(translate("inactive"))).toBeDefined();
         expect(screen.getByText(mockedValidator!.stakingBalance!.staked.toString() + " NEAR")).toBeDefined();
         expect(screen.getByTestId("UserCheckIcon")).toBeDefined();
+    });
+
+    test("Renders correctly with property showEdit", () => {
+        const mockedValidator = new ValidatorMock({ fee: 12 });
+        const mockOnEdit = jest.fn();
+
+        render(<ValidatorInformation validator={mockedValidator} showEdit onEdit={mockOnEdit} />);
+        expect(screen.getByTestId("UserCheckIcon")).toBeDefined();
+        expect(screen.getByText(mockedValidator.accountId)).toBeDefined();
+        expect(screen.getByText("12% " + capitalize(translate("fee")) + " - ")).toBeDefined();
+        expect(screen.getByText(translate("inactive"))).toBeDefined();
+
+        const editButton = screen.getByRole("button", { name: translate("edit") });
+        expect(editButton).toBeDefined();
+        fireEvent.press(editButton);
+        expect(mockOnEdit).toHaveBeenCalled();
     });
 });
