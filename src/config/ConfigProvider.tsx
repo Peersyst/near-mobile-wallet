6513bb3aa08ac3ab65de56fc81676b1a9e3a5120
JordiParraCrespo <jordiparra99@gmail.com>
2023-01-24 11:52:19 +0100

    feat(config): add locale to the config

--- src/config/ConfigProvider.tsx
+++ src/config/ConfigProvider.tsx
@@ -1,6 +1,8 @@
 import { ConfigProvider as GenesysConfigProvider } from "@peersyst/react-native-components";
 import { useTranslate } from "module/common/hook/useTranslate";
+import settingsState from "module/settings/state/SettingsState";
 import { ReactNode } from "react";
+import { useRecoilValue } from "recoil";
 import config from "./config";
 
 export interface ConfigProviderProps {
@@ -9,7 +11,8 @@ export interface ConfigProviderProps {
 
 const ConfigProvider = ({ children }: ConfigProviderProps): JSX.Element => {
     const translate = useTranslate("error");
-    return <GenesysConfigProvider config={{ ...config, translate }}>{children}</GenesysConfigProvider>;
+    const { locale = "en" } = useRecoilValue(settingsState);
+    return <GenesysConfigProvider config={{ ...config, translate, locale }}>{children}</GenesysConfigProvider>;
 };
 
 export default ConfigProvider;
