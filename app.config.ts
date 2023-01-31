61a97ebe2e61e2e78cfda2705493b3bd18c7767d
AdriaCarrera <adria.cm95@gmail.com>
2023-01-31 12:20:14 +0100

    Revert slug change

--- app.config.ts
+++ app.config.ts
@@ -3,7 +3,7 @@ import { ExpoConfig, ConfigContext } from "@expo/config";
 export default ({ config }: ConfigContext): ExpoConfig => ({
     ...config,
     name: "NEAR Mobile",
-    slug: "NEARMobile",
+    slug: "NEARMobileWallet",
     owner: "peersyst",
     version: "1.1.3",
     orientation: "portrait",
