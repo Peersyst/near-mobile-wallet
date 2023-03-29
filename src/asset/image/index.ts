585461683408a49577846dc2b1ddb7de9365f5eb
AgustinMJ <74896585+AgustinMJ@users.noreply.github.com>
2023-03-29 13:51:58 +0200

    Update assets

--- src/asset/image/index.ts
+++ src/asset/image/index.ts
@@ -1,20 +1,5 @@
 //@ts-nocheck
-export { default as empty_folder } from "./EmptyFolder.png";
-export { default as animation } from "./animation.webp";
-export { default as colored_background } from "./coloredBackground.png";
-export { default as deposit } from "./deposit.png";
-export { default as dollar_coin } from "./dollar-coin.png";
-export { default as euro_coin } from "./euro-coin.png";
-export { default as key } from "./key.png";
-export { default as lock } from "./lock.png";
 export { default as near_bg_0 } from "./near-bg-0.png";
 export { default as near_bg_1 } from "./near-bg-1.png";
 export { default as near_usn } from "./near-usn.png";
-export { default as nervos_coin } from "./nervos-coin.png";
-export { default as notes } from "./notes.png";
 export { default as placeholder_image } from "./placeholder_image.png";
-export { default as pound_coin } from "./pound-coin.png";
-export { default as send } from "./send.png";
-export { default as wallet } from "./wallet.png";
-export { default as yen_coin } from "./yen-coin.png";
-export { default as yuan_coin } from "./yuan-coin.png";
