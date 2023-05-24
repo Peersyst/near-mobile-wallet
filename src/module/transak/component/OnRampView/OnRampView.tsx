9650efaab2b05e35e28b4ba3dbc08f6a2d71b26e
JordiParraCrespo <jordiparra99@gmail.com>
2023-05-24 11:30:20 +0200

    fix: on ramb view styles improved

--- src/module/transak/component/OnRampView/OnRampView.tsx
+++ src/module/transak/component/OnRampView/OnRampView.tsx
@@ -21,22 +21,21 @@ export default function OnRampView() {
     };
 
     return (
-        <Col style={{ height: 1000, maxHeight: "100%" }}>
-            <TransakWebView
-                queryParams={{
-                    apiKey: "4a3f3484-0df6-41af-b865-6da3bc82ccd9",
-                    environment: "STAGING",
-                    defaultCryptoCurrency: "NEAR",
-                    networks: "mainnet",
-                    cryptoCurrencyList: "NEAR",
-                    walletAddress: "3f2133f0d24ffeb5df4c07d9b767fd66ac31fcf2acce18b4ee9c32c8ed8b3b97",
-                    // .....
-                    // For the full list of query params refer Props section below
-                }}
-                onTransakEventHandler={transakEventHandler}
+        <TransakWebView
+            queryParams={{
+                apiKey: "4a3f3484-0df6-41af-b865-6da3bc82ccd9",
+                environment: "STAGING",
+                defaultCryptoCurrency: "NEAR",
+                networks: "mainnet",
+                cryptoCurrencyList: "NEAR",
+                walletAddress: "3f2133f0d24ffeb5df4c07d9b767fd66ac31fcf2acce18b4ee9c32c8ed8b3b97",
                 // .....
-                // For the full list of react-native-webview props refer Props section below
-            />
-        </Col>
+                // For the full list of query params refer Props section below
+            }}
+            onTransakEventHandler={transakEventHandler}
+            style={{ flex: 1 }}
+            // .....
+            // For the full list of react-native-webview props refer Props section below
+        />
     );
 }
