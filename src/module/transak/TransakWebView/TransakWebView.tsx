06a185426af347bdf02d46bdd5b754163cdd1c2f
JordiParraCrespo <jordiparra99@gmail.com>
2023-05-30 16:49:29 +0200

    refactor(transak): remove transak package add only

--- src/module/transak/TransakWebView/TransakWebView.tsx
+++ src/module/transak/TransakWebView/TransakWebView.tsx
@@ -1,10 +1,17 @@
-//@ts-ignore
-import TransakSDK from "@transak/react-native-sdk";
+import { WebView } from "react-native-webview";
 import { TransakWebViewProps } from "./TransakWebView.types";
 import { memo } from "react";
 
 function TransakWebView(props: TransakWebViewProps): JSX.Element {
-    return <TransakSDK {...props} />;
+    //TODO: Remove this hardcoded uri
+    return (
+        <WebView
+            source={{
+                uri: "https://global.transak.com/?apiKey=f2d9a722-cac0-4bea-bdfc-2560d65f1a1e&defaultCryptoCurrency=NEAR&networks=mainnet&cryptoCurrencyList=NEAR",
+            }}
+            {...props}
+        />
+    );
 }
 
 export default memo(TransakWebView);
