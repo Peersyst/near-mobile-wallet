4dac946c149c02e783f211fd70d6ef5dd6c4ea21
AgustinMJ <agustinmillanjimenez+github@gmail.com>
2023-06-01 17:20:35 +0200

    fix: transak & useLoad

--- src/module/common/query/useLoad.ts
+++ src/module/common/query/useLoad.ts
@@ -3,10 +3,6 @@ import { useEffect, useState } from "react";
 import { SettingsStorage } from "module/settings/SettingsStorage";
 import settingsState, { defaultSettingsState } from "module/settings/state/SettingsState";
 import useRecoverWallets from "module/wallet/hook/useRecoverWallets";
-import { Chains } from "near-peersyst-sdk";
-import WalletController from "module/wallet/utils/WalletController";
-import { FlagsStorage } from "module/settings/FlagsStorage";
-import { WalletStorage } from "module/wallet/WalletStorage";
 
 export function useLoad(): boolean {
     const [loading, setLoading] = useState(true);
@@ -15,24 +11,11 @@ export function useLoad(): boolean {
 
     useEffect(() => {
         const getStorage = async () => {
-            const hasMnemonic = await WalletController.hasMnemonic();
-            const flags = await FlagsStorage.get();
             const settings = { ...defaultSettingsState, ...((await SettingsStorage.getAllSettings()) || {}) };
 
-            if (!hasMnemonic) await SettingsStorage.set(settings);
-            else {
-                // [MAINNET RELEASE]: Removes wallets and forces network to MAINNET when upgrading
-                if (!flags.forcedMainnet) {
-                    if (settings.network !== Chains.MAINNET) {
-                        settings.network = Chains.MAINNET;
-                        await SettingsStorage.set({ network: Chains.MAINNET });
-                    }
-                    await Promise.all([WalletStorage.clearAll(), FlagsStorage.set({ forcedMainnet: true })]);
-                } else {
-                    // If already forced to MAINNET and hasWallet = true, recover wallets
-                    await recoverWallets(settings.network);
-                }
-            }
+            const hasWallets = await recoverWallets(settings.network);
+
+            if (!hasWallets) await SettingsStorage.set(settings);
 
             setSettingsState(settings);
 
--- src/module/transak/TransakWebView/TransakWebView.tsx
+++ src/module/transak/TransakWebView/TransakWebView.tsx
@@ -2,19 +2,10 @@ import { WebView } from "react-native-webview";
 import { TransakWebViewProps } from "./TransakWebView.types";
 import { memo, useMemo } from "react";
 import parseQueryParams from "./utils/parseQueryParams";
-import { Platform } from "react-native";
 
 function TransakWebView({ queryParams, ...rest }: TransakWebViewProps): JSX.Element {
     const uri = useMemo(() => parseQueryParams(queryParams), [queryParams]);
-    return (
-        <WebView
-            source={{ uri }}
-            enableApplePay={Platform.OS === "ios"}
-            allowInlineMediaPlayback
-            mediaPlaybackRequiresUserAction={false}
-            {...rest}
-        />
-    );
+    return <WebView source={{ uri }} enableApplePay allowsInlineMediaPlayback mediaPlaybackRequiresUserAction={false} {...rest} />;
 }
 
 export default memo(TransakWebView);
