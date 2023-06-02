846b9bc5ad0a72a6e9d90f96856f9ffd1f4aff21
AgustinMJ <agustinmillanjimenez+github@gmail.com>
2023-06-02 10:15:35 +0200

    fix: Private key copy and transak modal

--- src/module/common/component/layout/BasePage/BasePage.tsx
+++ src/module/common/component/layout/BasePage/BasePage.tsx
@@ -3,12 +3,14 @@ import Header from "module/common/component/navigation/Header/Header";
 import { BasePageContent, BasePageRoot } from "./BasePage.styles";
 import { StatusBar } from "@peersyst/react-native-components";
 
-const BasePage = ({ children, header = true, style }: BasePageProps): JSX.Element => {
+const BasePage = ({ children, header = true, style, watchStatusBar = true }: BasePageProps): JSX.Element => {
     return (
         <>
             <BasePageRoot style={style}>
                 {header && <Header />}
-                <BasePageContent header={header}>{children}</BasePageContent>
+                <BasePageContent watchStatusBar={watchStatusBar} header={header}>
+                    {children}
+                </BasePageContent>
             </BasePageRoot>
             <StatusBar />
         </>
