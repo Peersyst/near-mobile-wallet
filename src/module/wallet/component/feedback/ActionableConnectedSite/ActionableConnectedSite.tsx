2f820d4fa113adcdb4d6bc190a2fbd9bd7a6af21
GuillemGarciaDev <guillem.garcia.gil@protonmail.com>
2023-08-14 15:27:06 +0200

    feat: Disconnect keys dialog

--- src/module/wallet/component/feedback/ActionableConnectedSite/ActionableConnectedSite.tsx
+++ src/module/wallet/component/feedback/ActionableConnectedSite/ActionableConnectedSite.tsx
@@ -2,20 +2,45 @@ import ConnectedSite from "../../display/ConnectedSite/ConnectedSite";
 import Actionable from "module/common/component/feedback/Actionable/Actionable";
 import { ConnectedSiteProps } from "../../display/ConnectedSite/ConnectedSite.types";
 import { useTranslate } from "module/common/hook/useTranslate";
-import useCancelableDialog from "module/common/hook/useCancelableDialog";
+
+import { useState } from "react";
+import { Dialog } from "@peersyst/react-native-components";
+import Typography from "module/common/component/display/Typography/Typography";
 
 export type ActionableConnectedSiteProps = Pick<ConnectedSiteProps, "site">;
 
 const ActionableConnectedSite = ({ site }: ActionableConnectedSiteProps) => {
     const translate = useTranslate();
+    const [openDialog, setOpenDialog] = useState(false);
 
-    const { showCancelableDialog } = useCancelableDialog();
-
-    const handleAction = () => showCancelableDialog({ title: translate("disconnect"), buttons: [] });
+    const handleAction = () => {
+        setOpenDialog(true);
+    };
 
     return (
         <Actionable onAction={handleAction} actionText={translate("disconnect")} actionProps={{ size: "sm", variant: "outlined" }} gap={12}>
             <ConnectedSite site={site} />
+            {openDialog && (
+                <Dialog
+                    content={
+                        <Typography variant="body2Strong" textAlign="center" style={{ marginBottom: 24 }}>
+                            Are your sure you want to remove access keys from Other site?
+                        </Typography>
+                    }
+                    buttons={[
+                        {
+                            type: "destructive",
+                            text: translate("disconnect"),
+                            onPress: () => setOpenDialog(false),
+                        },
+                        {
+                            variant: "text",
+                            text: translate("cancel"),
+                            onPress: () => setOpenDialog(false),
+                        },
+                    ]}
+                />
+            )}
         </Actionable>
     );
 };
