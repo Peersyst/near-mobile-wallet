832fac1a9d12c1d399cbed083d83154ec0962e19
GuillemGarciaDev <guillem.garcia.gil@protonmail.com>
2023-08-17 10:35:44 +0200

    fix: NetworkMismatchError on RequestSignerModal

--- src/module/signer/containers/RequestSignerModal/RequestSignerModal.tsx
+++ src/module/signer/containers/RequestSignerModal/RequestSignerModal.tsx
@@ -1,35 +1,29 @@
-import { Col, createModal, Skeleton, Typography, useModal } from "@peersyst/react-native-components";
+import { createModal, Skeleton, useModal } from "@peersyst/react-native-components";
 import CardSelectModal from "module/common/component/feedback/CardSelectModal/CardSelectModal";
-
 import SignRequestDetails from "module/signer/components/display/SignRequestDetails/SignRequestDetails";
 import { SignerModalProps } from "module/signer/hooks/useSignerModal";
 import useGetSignerRequest from "module/signer/queries/useGetSignerRequest";
 import useSelectedNetwork from "module/settings/hook/useSelectedNetwork";
 import SignModalScaffold from "module/signer/components/layout/SignModalScaffold/SignModalScaffold";
-import Error from "module/common/component/display/Error/Error";
+import NetworkMismatchError from "module/signer/components/feedback/NetworkMismatchError/NetworkMismatchError";
 
 const RequestSignerModal = createModal(({ id, ...modalProps }: SignerModalProps): JSX.Element => {
     const { hideModal } = useModal();
+    const network = useSelectedNetwork();
 
     const { data: signerRequest, isLoading } = useGetSignerRequest(id);
-
-    const network = useSelectedNetwork();
 
     const matchingNetwork = network === signerRequest?.network;
 
     const close = () => hideModal(RequestSignerModal.id);
 
     const handleSign = () => close();
-
     const handleReject = () => close();
 
     return (
         <CardSelectModal {...modalProps} title="Sign request" dismissal="close" style={{ height: "60%" }}>
             {!matchingNetwork ? (
-                <Error
-                    title="Network mismatch"
-                    description="This request is expecting a different network. Change the current network to another one to view request details."
-                />
+                <NetworkMismatchError />
             ) : (
                 <Skeleton loading={isLoading}>
                     <SignModalScaffold onSign={handleSign} onReject={handleReject}>
