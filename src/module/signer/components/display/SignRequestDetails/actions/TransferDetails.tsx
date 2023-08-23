3660775347fd459b15ce979857dae9b5309499fb
GuillemGarciaDev <guillem.garcia.gil@protonmail.com>
2023-08-23 09:26:22 +0200

    refactor: TransferDetails using ActionDetailsScaffold

--- src/module/signer/components/display/SignRequestDetails/actions/TransferDetails.tsx
+++ src/module/signer/components/display/SignRequestDetails/actions/TransferDetails.tsx
@@ -1,10 +1,9 @@
-import { Col } from "@peersyst/react-native-components";
 import { ActionDetailsProps } from "../SignRequestDetails.types";
-import Typography from "module/common/component/display/Typography/Typography";
 import { useTranslate } from "module/common/hook/useTranslate";
 import { TransferActionParams } from "../actions.types";
 import Balance from "module/wallet/component/display/Balance/Balance";
 import Container from "module/common/component/display/Container/Container";
+import ActionDetailsScaffold from "module/signer/components/layout/ActionDetailsScaffold/ActionDetailsScaffold";
 
 const TransferDetails = ({ params, receiverId }: ActionDetailsProps): JSX.Element => {
     const { deposit } = params as TransferActionParams;
@@ -12,19 +11,11 @@ const TransferDetails = ({ params, receiverId }: ActionDetailsProps): JSX.Elemen
     const translate = useTranslate();
 
     return (
-        <Col flex={1} gap={24} alignItems="center">
-            <Typography variant="h4Strong" textAlign="center">
-                {translate("transferAction")}
-            </Typography>
-            <Col gap={12}>
-                <Typography variant="body2Regular" textAlign="center">
-                    {translate("transferActionDescription", { receiverId })}
-                </Typography>
-                <Container>
-                    <Balance variant="body2Strong" units="token" balance={deposit} textAlign="center" />
-                </Container>
-            </Col>
-        </Col>
+        <ActionDetailsScaffold header={translate("transferAction")} description={translate("transferActionDescription", { receiverId })}>
+            <Container>
+                <Balance variant="body2Strong" units="token" balance={deposit} textAlign="center" />
+            </Container>
+        </ActionDetailsScaffold>
     );
 };
 
