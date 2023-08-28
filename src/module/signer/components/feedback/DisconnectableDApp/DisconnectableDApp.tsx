1ef8e8866e6f868931febe6ff51cb88a301b555d
GuillemGarciaDev <guillem.garcia.gil@protonmail.com>
2023-08-28 12:48:56 +0200

    fix: testing + props hook

--- src/module/signer/components/feedback/DisconnectableDApp/DisconnectableDApp.tsx
+++ src/module/signer/components/feedback/DisconnectableDApp/DisconnectableDApp.tsx
@@ -10,6 +10,9 @@ const DisconnectableDApp = ({ dapp }: DisconnectableDAppProps): JSX.Element => {
     const translate = useTranslate();
     const { actionableProps, swipeActionProps } = useDisconnectableDAppProps();
 
+    // TODO: When smart contracts loaded, check if dApp is connected
+    const dAppConnected = true;
+
     const { showCancelableDialog } = useCancelableDialog();
 
     const handleSwipeAction = () => {
@@ -30,9 +33,10 @@ const DisconnectableDApp = ({ dapp }: DisconnectableDAppProps): JSX.Element => {
             onSwipedAction={handleSwipeAction}
             swipedAction={translate("disconnect")}
             swipedActionProps={swipeActionProps}
+            enabled={dAppConnected}
             {...actionableProps}
         >
-            <DApp dapp={dapp} />
+            <DApp dapp={dapp} connected={dAppConnected} />
         </AnimatedActionable>
     );
 };
