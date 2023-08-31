db96ba341ef1ec01ca7e9758bad90834b6ad48f5
GuillemGarciaDev <guillem.garcia.gil@protonmail.com>
2023-08-31 11:00:27 +0200

    fix: useRecommendedDApps query + DAppTagSelect styles

--- src/module/signer/containers/RecommendedDApps/RecommendedDApps.tsx
+++ src/module/signer/containers/RecommendedDApps/RecommendedDApps.tsx
@@ -1,22 +1,21 @@
 import { Col } from "@peersyst/react-native-components";
 import DisconnectableDAppList from "module/signer/components/display/DisconnectableDAppList/DisconnectableDAppList";
 import useRecommendedDApps from "module/signer/hooks/useRecommendedDApps";
-import { Filters } from "./RecommendedDApps.styles";
-import DAppTagSelect from "module/signer/components/input/DAppTagSelect/DAppTagSelect";
+import { Filters, DAppTagFilter } from "./RecommendedDApps.styles";
 import useRecommendedDAppsFilters from "./hooks/useRecommendedDAppsFilters";
 import SearchBar from "module/common/component/input/SearchBar/SearchBar";
 
 const RecommendedDApps = (): JSX.Element => {
     const { filters, handleQueryChange, handleTagChange } = useRecommendedDAppsFilters();
-    const dapps = useRecommendedDApps(filters);
+    const { data: dapps, isLoading } = useRecommendedDApps(filters);
 
     return (
         <Col flex={1} style={{ width: "100%" }}>
             <Filters>
                 <SearchBar onChange={handleQueryChange} style={{ flex: 1 }} />
-                <DAppTagSelect value={filters.tag} onChange={handleTagChange} />
+                <DAppTagFilter value={filters.tag} onChange={handleTagChange} />
             </Filters>
-            <DisconnectableDAppList dapps={dapps} />
+            <DisconnectableDAppList dapps={dapps} loading={isLoading} />
         </Col>
     );
 };
--- src/module/signer/hooks/useRecommendedDApps.ts
+++ src/module/signer/hooks/useRecommendedDApps.ts
@@ -1,11 +1,19 @@
 import { config } from "config";
 import { RecommendedDAppsFilters } from "../containers/RecommendedDApps/RecommendedDApps.types";
+import { useQuery } from "react-query";
+import Queries from "../../../query/queries";
+import { DApp } from "../types";
 
 export default function useRecommendedDApps(props: RecommendedDAppsFilters) {
     const { query, tag: tagProp } = props;
-    const { recommendedDApps } = config.signerFeature;
-
-    return recommendedDApps.filter(
-        ({ name, tag }) => name.toLowerCase().includes(query.toLowerCase()) && (tagProp === "all" ? true : tagProp === tag),
+    return useQuery(
+        [Queries.RECOMMENDED_DAPPS, query, tagProp],
+        () =>
+            new Promise<DApp[]>((resolve) => {
+                const filteredRecommendedDapps = config.signerFeature.recommendedDApps.filter(
+                    ({ name, tag }) => name.toLowerCase().includes(query.toLowerCase()) && (tagProp === "all" ? true : tagProp === tag),
+                );
+                resolve(filteredRecommendedDapps);
+            }),
     );
 }
