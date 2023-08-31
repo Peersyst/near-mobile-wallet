04ae75d28eedca8f96337ff6448f224272b5dd5c
GuillemGarciaDev <guillem.garcia.gil@protonmail.com>
2023-08-31 11:53:59 +0200

    refactor: FunctionCall styles + NearMobileLogo added to preview with dynamic background

--- src/module/signer/components/display/ActionPreview/ActionPreview.tsx
+++ src/module/signer/components/display/ActionPreview/ActionPreview.tsx
@@ -2,11 +2,12 @@ import { Row } from "@peersyst/react-native-components";
 import { config } from "config";
 import { ActionPreviewProps } from "./ActionPreview.types";
 import { PreviewLogo, ActionPreviewRoot, ActionPreviewIcon } from "./ActionPreview.styles";
+import NearMobileLogo from "../NearMobileLogo/NearMobileLogo";
 
 const ActionPreview = ({ logoUrl, Icon }: ActionPreviewProps): JSX.Element => {
     return (
         <ActionPreviewRoot gap={12} justifyContent="center" alignItems="center">
-            <PreviewLogo source={{ uri: config.signerFeature.nearMobileLogoUrl }} />
+            <NearMobileLogo />
             {logoUrl && Icon && (
                 <Row gap={12} justifyContent="center" alignItems="center">
                     <ActionPreviewIcon as={Icon} />
