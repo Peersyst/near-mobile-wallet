34bcaa057da13ea6b540ffbf657e17d3a5ffd483
GuillemGarciaDev <guillem.garcia.gil@protonmail.com>
2023-09-26 12:02:50 +0200

    fix: removed unused styled component

--- src/module/common/component/navigation/BottomBar/BottomBarItem/BottomBarItem.styles.ts
+++ src/module/common/component/navigation/BottomBar/BottomBarItem/BottomBarItem.styles.ts
@@ -1,5 +1,5 @@
 import styled from "@peersyst/react-native-styled";
-import { Pressable } from "react-native";
+
 import { Icon, Typography } from "@peersyst/react-native-components";
 import { LinkItemIconProps, LinkTextProps } from "../BottomBar.types";
 
@@ -11,5 +11,3 @@ export const LinkItemIcon = styled(Icon)<LinkItemIconProps>(({ theme, isActive }
     color: isActive ? theme.palette.gray[theme.palette.mode === "light" ? 600 : 900] : theme.palette.overlay["40%"],
     fontSize: 20,
 }));
-
-export const BottomBarItemRoot = styled(Pressable)(() => ({}));
