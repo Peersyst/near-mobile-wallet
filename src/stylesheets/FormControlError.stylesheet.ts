16763a0b55d5a954b6acbf1bd024c3a626f09def
Longbo <longboqiu@hotmail.com>
2024-02-08 17:26:21 +0100

    fix: `FormControl` typography

--- src/stylesheets/FormControlError.stylesheet.ts
+++ src/stylesheets/FormControlError.stylesheet.ts
@@ -2,5 +2,5 @@ import { FormControlError } from "@peersyst/react-native-components";
 import { stylesheet } from "@peersyst/react-native-styled";
 
 export const formControlErrorStylesheet = stylesheet(FormControlError)(({ fromTheme }) => ({
-    fontSize: fromTheme("typography.body3.fontSize"),
+    fontSize: fromTheme("typography.body3Regular.fontSize"),
 }));
