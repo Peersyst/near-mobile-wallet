7d87af75b926f64c78691170d8f318758cce9fb0
Jordi Parra Crespo <jordiparra99@gmail.com>
2024-02-08 11:47:06 +0100

    fix(app): remove near usn icon

--- src/asset/image/index.ts
+++ src/asset/image/index.ts
@@ -1,5 +1,4 @@
 //@ts-nocheck
 export { default as near_bg_0 } from "./near-bg-0.png";
 export { default as near_bg_1 } from "./near-bg-1.png";
-export { default as near_usn } from "./near-usn.png";
 export { default as placeholder_image } from "./placeholder_image.png";
