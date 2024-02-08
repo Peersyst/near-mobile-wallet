a6cf20c0498e3e30ae1f8de4490266c876b54453
Longbo <longboqiu@hotmail.com>
2024-02-08 11:03:43 +0100

    fix: remove logs

--- src/module/api/OpenApiConfig.ts
+++ src/module/api/OpenApiConfig.ts
@@ -1,11 +1,4 @@
-console.log("BEFORE IMPORT CONFIG");
-
 import { config } from "../../config";
-
-console.log("AFTER IMPORT CONFIG");
-
 import { OpenAPI } from "./service";
 
-console.log("CONFIG", config);
-
-OpenAPI.BASE = "config.signerFeature.backendUrl";
+OpenAPI.BASE = config.signerFeature.backendUrl;
