c1cb49cc8feb043efde1ac86b3cee9aa30c27a5b
Longbo <longboqiu@hotmail.com>
2024-02-08 13:16:02 +0100

    feat: remove unused component

--- src/stylesheets/Divider.stylesheet.ts
+++
@@ -1,6 +0,0 @@
-import { Divider } from "@peersyst/react-native-components";
-import { stylesheet } from "@peersyst/react-native-styled";
-
-export const dividerStylesheet = stylesheet(Divider)(({ fromTheme }) => ({
-    backgroundColor: fromTheme("palette.overlay[8%]"),
-}));
