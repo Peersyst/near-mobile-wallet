8b145110e51a3df8e98a0fdd61bb6a8ddbc2ffa8
AgustinMJ <agustinmillanjimenez+github@gmail.com>
2024-02-09 12:28:04 +0100

    fix: Fix tests ci

--- jest.config.js
+++ jest.config.js
@@ -9,7 +9,7 @@ const { compilerOptions } = require("./tsconfig.path");
 module.exports = {
     testEnvironment: "jest-environment-jsdom",
     preset: "jest-expo",
-    setupFilesAfterEnv: ["<rootDir>/test/setup.tsx", "@testing-library/jest-native/extend-expect"],
+    setupFilesAfterEnv: ["@testing-library/jest-native/extend-expect"],
     modulePaths: [
         "node_modules",
         "utils", // a utility folder
@@ -21,38 +21,6 @@ module.exports = {
     transformIgnorePatterns: [
         "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|@peersyst|@ckb-lumos/hd/node_modules/uuid)",
     ],
-    collectCoverageFrom: [
-        "./src/**/*.(ts|js|tsx|jsx)",
-        "!./src/**/index.(ts|js|tsx|jsx)",
-        "!./src/**/*.d.ts",
-        "!./src/**/mock/**/*",
-        "!./src/module/api/**/*",
-        "!./src/script/**/*",
-        "!./src/**/*.(styles|types).(ts|js|tsx|jsx)",
-        "!./src/**/*Provider.tsx",
-        "!./src/module/common/icons/**/*",
-        "!./src/module/common/style/**/*",
-        "!./src/module/common/service/BaseStorageService.ts",
-        "!./src/module/common/hook/(useCachedResources.ts|useColorScheme.ts)",
-        "!./src/Providers.tsx",
-        "!./src/Navigator.tsx",
-        "!./src/**/*NavigatorGroup.(tsx|ts)",
-        "!./src/Stack.ts",
-        "!./src/utils/(isWeb|extractTextStyles).ts",
-        "!./src/module/common/component/layout/PagerView/*",
-        "!./src/module/common/component/layout/BasePage/*",
-        "!./src/module/common/component/layout/LogoPage/*",
-        "!./src/module/common/service/CkbSdkService.ts",
-        "!./src/module/sdk/**/*",
-        "!./src/**/query/**/*",
-    ],
-    coverageDirectory: "./coverage",
-    coverageThreshold: {
-        global: {
-            branches: 50,
-            statements: 50,
-        },
-    },
     moduleNameMapper: {
         ...pathsToModuleNameMapper(compilerOptions.paths || {}, { prefix: resolve(compilerOptions.baseUrl) }),
         uuid: "<rootDir>/test/__mocks__/fileMock.js",
