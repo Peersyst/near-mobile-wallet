bfa425775f710c63f4cbe60aa81cfd3fdbb60293
Longbo <longboqiu@hotmail.com>
2024-02-12 11:09:58 +0100

    feat: add `RepositoryFactory` and errors

---
+++ src/refactor/data-access/repository/error/RepositoryErrorCodes.ts
@@ -0,0 +1,6 @@
+// Merge all module error codes here
+const RepositoryErrorCodes = {};
+
+export type RepositoryErrorCode = keyof typeof RepositoryErrorCodes;
+
+export default RepositoryErrorCodes;
---
+++ src/refactor/domain/adapter/RepositoryFactory.ts
@@ -0,0 +1,5 @@
+import Factory from "refactor/common/utils/Factory";
+
+export interface IRepositoryFactory {}
+
+export default Factory<IRepositoryFactory>({});
