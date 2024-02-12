ab9485fe6fa80e4d28b0383a1432a0c9d969622d
Longbo <longboqiu@hotmail.com>
2024-02-12 11:49:26 +0100

    fix: imports

--- src/refactor/ui/locale/index.ts
+++ src/refactor/ui/locale/index.ts
@@ -1,4 +1,4 @@
-export * from "./i18n.types";
 export * from "./i18n";
 export * from "./pluguins/LanguageDetectorPlugin/LanguageDetectorPlugin";
 export * from "./utils";
+export * from "./i18n.types.d";
