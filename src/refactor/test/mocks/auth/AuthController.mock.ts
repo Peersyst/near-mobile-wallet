b6a7df27c79f5b3feac702249916e8d1b7494fac
pablo <silvap.javier@gmail.com>
2024-02-16 11:36:37 +0100

    Fix test

--- src/refactor/test/mocks/auth/AuthController.mock.ts
+++ src/refactor/test/mocks/auth/AuthController.mock.ts
@@ -5,7 +5,6 @@ import { AuthStateMock } from "./authState.mock";
 
 export default createMock<IAuthController>({
     signUp: new MethodMock("mockResolvedValue", true),
-    recover: new MethodMock("mockResolvedValue", true),
     login: new MethodMock("mockResolvedValue", true),
     logout: new MethodMock("mockResolvedValue", true),
     authState: new AuthStateMock(),
