2730a210ce415a2c7a23018ef346c688effbf68a
AdriaCarrera <adria.cm95@gmail.com>
2024-02-16 14:58:23 +0100

    bump app version

--- app.config.ts
+++ app.config.ts
@@ -5,7 +5,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
     name: "NEAR Mobile",
     slug: "NEARMobileWallet",
     owner: "peersyst",
-    version: "1.4.2",
+    version: "1.4.3",
     orientation: "portrait",
     icon: "./assets/images/near-icon.png",
     scheme: "near-mobile-wallet",
