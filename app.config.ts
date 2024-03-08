2430dcf0529ab8f5e125ebe4b47fa0aa05914dc5
Adrià Carrera <adria.cm95@gmail.com>
2024-03-08 10:14:22 +0100

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -5,7 +5,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
     name: "NEAR Mobile",
     slug: "NEARMobileWallet",
     owner: "peersyst",
-    version: "1.5.0",
+    version: "1.5.1",
     orientation: "portrait",
     icon: "./assets/images/near-icon.png",
     scheme: "near-mobile-wallet",
