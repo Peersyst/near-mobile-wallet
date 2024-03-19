05f9e6b01ee17fdcdfa451336a74d0b419e5baa3
pablo <silvap.javier@gmail.com>
2024-03-19 13:33:14 +0100

    Fixs

--- src/module/common/component/input/IconButton/IconButton.types.ts
+++
@@ -1,5 +0,0 @@
-import { IconButtonProps as BaseIconButtonProps } from "@peersyst/react-native-components";
-
-export interface IconButtonProps extends BaseIconButtonProps {
-    variant?: "primary" | "transparent";
-}
--- src/module/common/component/input/LabeledIconButton/LabeledIconButton.tsx
+++ src/module/common/component/input/LabeledIconButton/LabeledIconButton.tsx
@@ -1,21 +1,23 @@
-import { Col } from "@peersyst/react-native-components";
+import { Col, ElementStyler } from "@peersyst/react-native-components";
 import { Placeholder } from "../../display/PinDisplay/PinDisplay.styles";
 import { TouchableWithoutFeedback } from "react-native-gesture-handler";
-import IconButton from "../IconButton/IconButton";
-import { IconButtonProps } from "../IconButton/IconButton.types";
+import { LabeledIconButtonButton } from "./LabeledIconButton.styles";
+import { ButtonProps } from "../Button/Button.types";
+import { ReactElement } from "react";
 
-export interface LabeledIconButtonProps extends Omit<IconButtonProps, "onPress"> {
+export interface LabeledIconButtonProps extends Omit<ButtonProps, "onPress" | "children"> {
     label: string;
     onPress: () => void;
+    children: ReactElement;
 }
 
 const LabeledIconButton = ({ label, children, variant, onPress }: LabeledIconButtonProps): JSX.Element => {
     return (
         <TouchableWithoutFeedback onPress={onPress}>
             <Col gap={6} alignItems="center">
-                <IconButton variant={variant} style={{ fontSize: 24 }}>
-                    {children}
-                </IconButton>
+                <LabeledIconButtonButton variant={variant}>
+                    <ElementStyler style={{ fontSize: 24 }}>{children}</ElementStyler>
+                </LabeledIconButtonButton>
                 <Placeholder variant="body3Strong">{label}</Placeholder>
             </Col>
         </TouchableWithoutFeedback>
