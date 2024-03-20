a743088fd56ebf78e4d28a0b68d0eefd70a24f2f
pablo <silvap.javier@gmail.com>
2024-03-20 14:12:35 +0100

    Fix color

--- src/module/wallet/component/core/WalletsBackupModal/WalletsBackupAdvise/WalletsBackupAdvise.styles.ts
+++ src/module/wallet/component/core/WalletsBackupModal/WalletsBackupAdvise/WalletsBackupAdvise.styles.ts
@@ -2,6 +2,6 @@ import { IconButton } from "@peersyst/react-native-components";
 import styled from "@peersyst/react-native-styled";
 
 export const WalletsBackupAdviseIcon = styled(IconButton)(({ theme }) => ({
-    color: theme.palette.blue,
+    color: theme.palette.primary,
     fontSize: 40,
 }));
