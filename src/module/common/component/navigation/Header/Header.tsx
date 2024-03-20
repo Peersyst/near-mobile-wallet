1ea0c0ce342202f936dff7845ef969d2c996be2e
Jordi Parra Crespo <jordiparra99@gmail.com>
2024-03-20 09:52:03 +0100

    fix: improve header styles

--- src/module/common/component/navigation/Header/Header.tsx
+++ src/module/common/component/navigation/Header/Header.tsx
@@ -1,25 +1,23 @@
 import { SettingsIcon } from "icons";
 import LinearLogo from "../../display/LinearBgLogo/LinearBgLogo";
-import { HeaderRoot, HeaderSettingsButton } from "./Header.styles";
+import { HeaderNetworkChip, HeaderRoot, HeaderSettingsButton } from "./Header.styles";
 import { Row } from "@peersyst/react-native-components";
 import useNavigation from "module/common/hook/useNavigation";
 import { MainScreens } from "../MainNavigatorGroup/MainScreens";
 import useWalletGradient from "module/wallet/hook/useWalletGradient";
-import SelectNetworkChip from "module/settings/components/core/SelectNetworkChip/SelectNetworkChip";
 
 const Header = (): JSX.Element => {
     const navigation = useNavigation();
     const [startColor, endColor] = useWalletGradient();
+
     return (
         <HeaderRoot>
             <Row alignItems="center" justifyContent="center" flex={1}>
-                <SelectNetworkChip style={{ position: "absolute", left: 8 }} />
+                <HeaderNetworkChip />
                 <LinearLogo startColor={startColor} endColor={endColor} />
-                <Row style={{ position: "absolute", right: 8 }}>
-                    <HeaderSettingsButton onPress={() => navigation.navigate(MainScreens.SETTINGS)}>
-                        <SettingsIcon />
-                    </HeaderSettingsButton>
-                </Row>
+                <HeaderSettingsButton onPress={() => navigation.navigate(MainScreens.SETTINGS)}>
+                    <SettingsIcon />
+                </HeaderSettingsButton>
             </Row>
         </HeaderRoot>
     );
