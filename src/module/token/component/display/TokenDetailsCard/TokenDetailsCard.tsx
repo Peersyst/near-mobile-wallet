c8b1cf1a4576259c70a78eefefefeaf996fc5c8f
pablo <silvap.javier@gmail.com>
2024-03-21 15:07:30 +0100

    Fixs comments

--- src/module/token/component/display/TokenDetailsCard/TokenDetailsCard.tsx
+++ src/module/token/component/display/TokenDetailsCard/TokenDetailsCard.tsx
@@ -1,4 +1,4 @@
-import { Row } from "@peersyst/react-native-components";
+import { Row, useModal } from "@peersyst/react-native-components";
 import useTranslate from "module/common/hook/useTranslate";
 import { Token } from "near-peersyst-sdk";
 import { capitalize } from "@peersyst/react-utils";
@@ -6,6 +6,8 @@ import TokenBalance from "../../display/TokenBalance/TokenBalance";
 import useGetSwapLink from "module/common/hook/useGetSwapLink";
 import { Linking } from "react-native";
 import { TokenDetailsCardButton, TokenDetailsCardRoot } from "./TokenDetailsCard.styles";
+import SendModal from "module/transaction/component/core/SendModal/SendModal";
+import { AssetType } from "module/wallet/wallet.types";
 
 export interface TokenDetailsCardProps {
     token: Token;
@@ -14,11 +16,22 @@ export interface TokenDetailsCardProps {
 const TokenDetailsCard = ({ token }: TokenDetailsCardProps): JSX.Element => {
     const translate = useTranslate();
     const uriSwap = useGetSwapLink({ contractId: token.contractId });
+    const { showModal } = useModal();
     return (
         <TokenDetailsCardRoot gap={16} textAlign="center">
-            <TokenBalance variant="body3Strong" token={token} alignItems="center" />
+            <TokenBalance
+                balanceProps={{ variant: "body3Strong", textAlign: "right" }}
+                fiatBalanceProps={{ variant: "body4Strong", light: true }}
+                token={token}
+                alignItems="center"
+            />
             <Row gap={4} justifyContent="center">
-                <TokenDetailsCardButton variant="secondary" size="md" fullWidth>
+                <TokenDetailsCardButton
+                    variant="secondary"
+                    size="md"
+                    fullWidth
+                    onPress={() => showModal(SendModal, { defaultAsset: { type: AssetType.FT, ft: token } })}
+                >
                     {capitalize(translate("send"))}
                 </TokenDetailsCardButton>
                 <TokenDetailsCardButton variant="secondary" size="md" fullWidth onPress={() => Linking.openURL(uriSwap)}>
