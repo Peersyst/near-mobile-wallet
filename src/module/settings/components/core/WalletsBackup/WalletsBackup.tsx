fc119a55cd09d7ab8f9c5129bd630fc9c8cf15f8
pablo <silvap.javier@gmail.com>
2024-03-21 15:30:08 +0100

    [APP] feat: add skip backup and create isBackupDone state for control

--- src/module/settings/components/core/WalletsBackup/WalletsBackup.tsx
+++ src/module/settings/components/core/WalletsBackup/WalletsBackup.tsx
@@ -2,16 +2,11 @@ import WalletsBackupModal from "module/wallet/component/core/WalletsBackupModal/
 import SettingsMenuItem from "module/settings/components/navigation/SettingsMenuItem/SettingsMenuItem";
 import { useModal } from "@peersyst/react-native-components";
 import useTranslate from "module/common/hook/useTranslate";
-import useWalletState from "module/wallet/hook/useWalletState";
 
 const WalletsBackup = () => {
     const { showModal } = useModal();
     const translate = useTranslate();
-    const {
-        state: { isBackupDone },
-    } = useWalletState();
 
-    console.log("isBackupDone", isBackupDone);
     return <SettingsMenuItem text={translate("back_up_your_accounts")} onPress={() => showModal(WalletsBackupModal)} />;
 };
 
