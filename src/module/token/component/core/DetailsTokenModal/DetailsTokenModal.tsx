db1c73de8653d0cb735327efb796f6c7fec54f51
pablo <silvap.javier@gmail.com>
2024-03-21 15:10:42 +0100

    Fixs lint

--- src/module/token/component/core/DetailsTokenModal/DetailsTokenModal.tsx
+++ src/module/token/component/core/DetailsTokenModal/DetailsTokenModal.tsx
@@ -3,26 +3,15 @@ import CardNavigatorModal from "module/common/component/navigation/CardNavigator
 import Typography from "module/common/component/display/Typography/Typography";
 import { useControlled } from "@peersyst/react-hooks";
 import { Token } from "near-peersyst-sdk";
-import { capitalize } from "@peersyst/react-utils";
-import TokenBalance from "../../display/TokenBalance/TokenBalance";
-import useGetSwapLink from "module/common/hook/useGetSwapLink";
-import { Linking } from "react-native";
-import { useModal } from "@peersyst/react-native-components";
-import SendModal from "module/transaction/component/core/SendModal/SendModal";
-import { AssetType } from "module/wallet/wallet.types";
 import TokenDetailsCard from "../../display/TokenDetailsCard/TokenDetailsCard";
 import TokenNameWithIcon from "../../display/TokenNameWithIcon/TokenNameWithIcon";
-import useTranslate from "module/common/hook/useTranslate";
 
 export interface DetailsTokenModalProps extends ExposedBackdropProps {
     token: Token;
 }
 
 const DetailsTokenModal = createBackdrop<DetailsTokenModalProps>(({ token, open: openProp, defaultOpen = true, ...rest }) => {
-    const translate = useTranslate();
     const [open, setOpen] = useControlled(defaultOpen, openProp);
-    const uriSwap = useGetSwapLink();
-    const { showModal } = useModal();
     return (
         <CardNavigatorModal
             open={open}
