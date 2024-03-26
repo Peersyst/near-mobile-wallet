87a08b3785dacbb4de09874af106c4057f164c6a
pablo <silvap.javier@gmail.com>
2024-03-26 11:33:50 +0100

    remove style

--- src/module/wallet/component/core/WalletsBackupModal/WalletsBackupAdviseAlertContent/WalletsBackupAdviseAlertContent.styles.ts
+++
@@ -1,7 +0,0 @@
-import { Typography } from "@peersyst/react-native-components";
-import styled from "@peersyst/react-native-styled";
-import { DeviceSize } from "module/common/hook/useDeviceSize";
-
-export const TypographyAdvise = styled(Typography)<{ size: DeviceSize }>(({ size }) => ({
-    fontSize: size === DeviceSize.SMALL ? 11 : 14,
-}));
