e113bb1bd54ff67e62ecc3ce7c22b9312088170e
pablo <silvap.javier@gmail.com>
2024-03-26 18:29:25 +0100

    [APP] feat : OutOfNear component

---
+++ src/module/wallet/component/display/YouDontHaveNearCard/YouDontHaveNearCard.styles.ts
@@ -0,0 +1,14 @@
+import styled from "@peersyst/react-native-styled";
+import { alpha } from "@peersyst/react-utils";
+import Typography from "module/common/component/display/Typography/Typography";
+import Card from "module/common/component/surface/Card/Card";
+
+export const YouDontHaveNearCardCardRoot = styled(Card)(() => ({
+    gap: 10,
+    textAlign: "center",
+}));
+
+export const DescriptionTypography = styled(Typography)(({ theme }) => ({
+    color: theme.palette.mode === "dark" ? theme.palette.text : alpha(theme.palette.gray[900], 0.6),
+    textAlign: "center",
+}));
