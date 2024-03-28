a8a9a73217379599e8b585d44157e4b74f90cb99
Jordi Parra Crespo <jordiparra99@gmail.com>
2024-03-28 13:14:53 +0100

    fix(app): fix layout and locales

--- src/module/transaction/component/core/AddNearModal/AddNearModalOption/AddNearModalOption.tsx
+++ src/module/transaction/component/core/AddNearModal/AddNearModalOption/AddNearModalOption.tsx
@@ -1,16 +1,13 @@
-import { Row, Typography } from "@peersyst/react-native-components";
-import { TouchableWithoutFeedback } from "react-native";
-import { AddNearModalOptionContentIcon } from "./AddNearModalOption.styles";
+import { Typography } from "@peersyst/react-native-components";
+import { AddNearModalOptionContentIcon, AddNearModalOptionRoot } from "./AddNearModalOption.styles";
 import { AddNearModalOptionProps } from "./AddNearModalOption.types";
 
 const AddNearModalOption = ({ text, onPress, icon }: AddNearModalOptionProps): JSX.Element => {
     return (
-        <TouchableWithoutFeedback onPress={onPress} accessibilityRole={"button"}>
-            <Row gap={12}>
-                <AddNearModalOptionContentIcon>{icon}</AddNearModalOptionContentIcon>
-                <Typography variant="body2Regular">{text}</Typography>
-            </Row>
-        </TouchableWithoutFeedback>
+        <AddNearModalOptionRoot onPress={onPress}>
+            <AddNearModalOptionContentIcon>{icon}</AddNearModalOptionContentIcon>
+            <Typography variant="body2Regular">{text}</Typography>
+        </AddNearModalOptionRoot>
     );
 };
 
--- src/module/wallet/component/core/WalletCard/WalletCardButtons/WalletCardButtons.tsx
+++ src/module/wallet/component/core/WalletCard/WalletCardButtons/WalletCardButtons.tsx
@@ -1,24 +1,24 @@
 import SendModal from "module/transaction/component/core/SendModal/SendModal";
-import { Row, useConfig, useModal } from "@peersyst/react-native-components";
+import { Row, useModal } from "@peersyst/react-native-components";
 import ReceiveModal from "module/transaction/component/core/ReceiveModal/ReceiveModal";
 import useTranslate from "module/common/hook/useTranslate";
 import { capitalize } from "@peersyst/react-utils";
-import useIsMainnet from "module/settings/hook/useIsMainnet";
 import useNavigation from "module/common/hook/useNavigation";
 import { MainScreens } from "module/common/component/navigation/MainNavigatorGroup/MainScreens";
 import { Linking } from "react-native";
 import { ArrowReceiveIcon, ArrowSendIcon, BuyIcon, SwapIcon } from "icons";
 import LabeledIconButton from "module/common/component/input/LabeledIconButton/LabeledIconButton";
 import useGetSwapLink from "module/common/hook/useGetSwapLink";
+import useIsBuyEnabled from "module/wallet/hook/useIsBuyEnabled";
+import AddNearModal from "module/transaction/component/core/AddNearModal/AddNearModal";
 
 const WalletCardButtons = (): JSX.Element => {
     const { showModal } = useModal();
     const translate = useTranslate();
     const navigate = useNavigation();
-    const enableBuy = useConfig("enableBuy");
-    const isMainnet = useIsMainnet();
-    const showBuyButton = enableBuy && isMainnet;
+    const showBuyButton = useIsBuyEnabled();
     const uriSwap = useGetSwapLink();
+
     return (
         <Row gap={6}>
             {showBuyButton && (
@@ -30,7 +30,7 @@ const WalletCardButtons = (): JSX.Element => {
                     <BuyIcon />
                 </LabeledIconButton>
             )}
-            <LabeledIconButton variant="secondary" label={capitalize(translate("send"))} onPress={() => showModal(SendModal)}>
+            <LabeledIconButton variant="secondary" label={capitalize(translate("send"))} onPress={() => showModal(AddNearModal)}>
                 <ArrowSendIcon />
             </LabeledIconButton>
             <LabeledIconButton variant="secondary" label={capitalize(translate("receive"))} onPress={() => showModal(ReceiveModal)}>
