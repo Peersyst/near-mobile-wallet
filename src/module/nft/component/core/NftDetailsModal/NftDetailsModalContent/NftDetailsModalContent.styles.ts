111a96809e4c6b82f4298a8a2b73fd16b16fe0d4
pablo <silvap.javier@gmail.com>
2024-04-01 20:10:40 +0200

    Fixes

--- src/module/nft/component/core/NftDetailsModal/NftDetailsModalContent/NftDetailsModalContent.styles.ts
+++ src/module/nft/component/core/NftDetailsModal/NftDetailsModalContent/NftDetailsModalContent.styles.ts
@@ -4,6 +4,6 @@ import NftImage from "module/nft/component/display/NftImage/NftImage";
 
 export const NftDetailsModalContentImage = styled(NftImage)(({ dimensions }) => ({
     width: dimensions.width - CARD_MODAL_PADDING * 2,
-    height: dimensions.width - CARD_MODAL_PADDING * 8,
-    objectFit: "contain",
+    height: dimensions.width - CARD_MODAL_PADDING * 2,
+    objectFit: "cover",
 }));
