1ddff4c2299c35912bf95f3ae3dfbe4006c23c19
Jordi Parra Crespo <jordiparra99@gmail.com>
2024-04-09 10:00:58 +0200

    style(app): remove unused component

--- src/module/wallet/screen/PickWalletMnemonicScreen/PickWalletMnemonicScreen.styles.ts
+++
@@ -1,4 +0,0 @@
-import styled from "@peersyst/react-native-styled";
-import { Col } from "@peersyst/react-native-components";
-
-export const PickWalletMnemonicScreenRoot = styled(Col, { gap: 24 })(() => ({}));
--- src/module/wallet/screen/PickWalletMnemonicScreen/PickWalletMnemonicScreen.tsx
+++ src/module/wallet/screen/PickWalletMnemonicScreen/PickWalletMnemonicScreen.tsx
@@ -3,7 +3,6 @@ import MnemonicPicker from "module/wallet/component/input/MnemonicPicker/Mnemoni
 import { Col, useToast } from "@peersyst/react-native-components";
 import useTranslate from "module/common/hook/useTranslate";
 import Advise from "module/common/component/display/Advise/Advise";
-import { PickWalletMnemonicScreenRoot } from "module/wallet/screen/PickWalletMnemonicScreen/PickWalletMnemonicScreen.styles";
 import Button from "module/common/component/input/Button/Button";
 
 export interface PickWalletMnemonicScreenProps {
