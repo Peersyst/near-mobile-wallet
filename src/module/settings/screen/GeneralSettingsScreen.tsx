bd1ef2e0b9af06911eae1e0efe825573d517ef40
pablo <silvap.javier@gmail.com>
2024-04-09 10:34:21 +0200

    Fix SettingsScreenContentRoot

--- src/module/settings/screen/GeneralSettingsScreen.tsx
+++ src/module/settings/screen/GeneralSettingsScreen.tsx
@@ -1,17 +1,17 @@
-import { Col } from "@peersyst/react-native-components";
 import SelectFiat from "../components/core/SelectFiat/SelectFiat";
 import SelectLocale from "../components/core/SelectLocale/SelectLocale";
 import SelectNetwork from "../components/core/SelectNetwork/SelectNetwork";
 import SwitchTheme from "../components/core/SwitchTheme/SwitchTheme";
+import { SettingsScreenContentRoot } from "./SettingsScreen.styles";
 
 const GeneralSettingsScreen = (): JSX.Element => {
     return (
-        <Col gap={12} style={{ marginTop: 20 }}>
+        <SettingsScreenContentRoot>
             <SwitchTheme />
             <SelectNetwork />
             <SelectFiat />
             <SelectLocale />
-        </Col>
+        </SettingsScreenContentRoot>
     );
 };
 
--- src/module/settings/screen/SecuritySettingsScreen.tsx
+++ src/module/settings/screen/SecuritySettingsScreen.tsx
@@ -1,22 +1,23 @@
-import { Col, useConfig } from "@peersyst/react-native-components";
+import { useConfig } from "@peersyst/react-native-components";
 import ChangePasscode from "module/settings/components/core/ChangePassCode/ChangePasscode";
 import WalletsBackup from "module/settings/components/core/WalletsBackup/WalletsBackup";
 import DeleteData from "module/settings/components/core/DeleteData/DeleteData";
 import BiometricsSwitch from "module/settings/components/core/BiometricsSwitch/BiometricsSwitch";
 import ConnectedSites from "../components/core/ConnectedSites/ConnectedSites";
 import ManualAccountImport from "../components/core/ManualAccountImport/ManualAccountImport";
+import { SettingsScreenContentRoot } from "./SettingsScreen.styles";
 
 const SecuritySettingsScreen = (): JSX.Element => {
     const enableManualImport = useConfig("enableManualImport");
     return (
-        <Col gap={12} style={{ marginTop: 20 }}>
+        <SettingsScreenContentRoot>
             <BiometricsSwitch />
             <ChangePasscode />
             <ConnectedSites />
             <WalletsBackup />
             {enableManualImport && <ManualAccountImport />}
             <DeleteData />
-        </Col>
+        </SettingsScreenContentRoot>
     );
 };
 
