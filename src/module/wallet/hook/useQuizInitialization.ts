5d3d5ef8ae3989bb9212fd53d09b496bd80b6216
pablo <silvap.javier@gmail.com>
2024-07-24 12:35:26 +0200

    Fixs

--- src/module/wallet/hook/useQuizInitialization.ts
+++ src/module/wallet/hook/useQuizInitialization.ts
@@ -1,25 +1,34 @@
-import { useState, useMemo } from "react";
-import { config } from "config";
+import { useMemo } from "react";
+import { useConfig } from "@peersyst/react-native-components";
 import { QuizQuestion } from "../component/core/WalletsBackupModal/WalletQuizBackup/WalletQuizBackup.stypes";
 
+function shuffleArray<T>(array: T[]) {
+    return array.sort(() => 0.5 - Math.random());
+}
+
+const initializeQuizQuestions = (questions: QuizQuestion[], length: number) => {
+    const shuffledQuestions = shuffleArray([...questions]);
+    const originalShuffledQuestions = shuffledQuestions.slice(0, length);
+    return originalShuffledQuestions;
+};
+
+const initializeQuizQuestionsShuffled = (questions: QuizQuestion[], length: number) => {
+    const originalShuffledQuestions = initializeQuizQuestions(questions, length);
+    return originalShuffledQuestions.map((question) => {
+        const shuffledResponses = shuffleArray([...question.responses]);
+        return { ...question, responses: shuffledResponses };
+    });
+};
+
 const useQuizInitialization = () => {
-    const [quizQuestions, setQuizQuestions] = useState<QuizQuestion[]>([]);
-    const [quizQuestionsShuffled, setQuizQuestionsShuffled] = useState<QuizQuestion[]>([]);
+    const questions = useConfig("securityQuizList");
+    const quizNumberOfQuestions = useConfig("quizNumberOfQuestions");
 
-    useMemo(() => {
-        const shuffledQuestions = [...config.securityQuizList].sort(() => 0.5 - Math.random());
-        const originalShuffledQuestions = shuffledQuestions.slice(0, config.quizNumberOfQuestions);
-        setQuizQuestions(originalShuffledQuestions);
-
-        const shuffledQuestionsWithShuffledResponses = originalShuffledQuestions.map((question) => {
-            const shuffledResponses = [...question.responses].sort(() => 0.5 - Math.random());
-            return { ...question, responses: shuffledResponses };
-        });
-
-        setQuizQuestionsShuffled(shuffledQuestionsWithShuffledResponses);
-    }, []);
-
-    return { quizQuestions, quizQuestionsShuffled };
+    return useMemo(() => {
+        const quizQuestions = initializeQuizQuestions(questions, quizNumberOfQuestions);
+        const quizQuestionsShuffled = initializeQuizQuestionsShuffled(questions, quizNumberOfQuestions);
+        return { quizQuestions, quizQuestionsShuffled };
+    }, [questions, quizNumberOfQuestions]);
 };
 
 export default useQuizInitialization;
