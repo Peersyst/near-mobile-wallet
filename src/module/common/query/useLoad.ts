7926a024d42618255693a675ad33592d35acf1eb
Agustín Millán Jiménez <74896585+AgustinMJ@users.noreply.github.com>
2025-02-10 12:04:12 +0100

    chore: trace load error

--- src/module/common/query/useLoad.ts
+++ src/module/common/query/useLoad.ts
@@ -5,6 +5,10 @@ import settingsState, { defaultSettingsState } from "module/settings/state/Setti
 import useRecoverWallets from "module/wallet/hook/useRecoverWallets";
 import WalletController from "module/wallet/utils/WalletController";
 import walletState from "../../wallet/state/WalletState";
+import { Alert } from "react-native";
+
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import * as ExpoSecureStore from "expo-secure-store";
 
 export function useLoad(): boolean {
     const [loading, setLoading] = useState(true);
@@ -14,25 +18,44 @@ export function useLoad(): boolean {
 
     useEffect(() => {
         const getStorage = async () => {
-            const hasMnemonic = await WalletController.hasMnemonic();
-            const isBackupDone = await WalletController.getIsBackupDone();
-            const settings = { ...defaultSettingsState, ...((await SettingsStorage.getAllSettings()) || {}) };
-            if (!hasMnemonic) await SettingsStorage.set(settings);
-            // Do not await this so the user can enter the app instantly with a loading state
-            else {
-                setWalletState((state) => ({
-                    ...state,
-                    loading: true,
-                    hasWallet: true,
-                    isBackupDone,
-                }));
+            try {
+                const hasMnemonic = await WalletController.hasMnemonic();
+                const isBackupDone = await WalletController.getIsBackupDone();
+                const settings = { ...defaultSettingsState, ...((await SettingsStorage.getAllSettings()) || {}) };
+                if (!hasMnemonic) await SettingsStorage.set(settings);
+                // Do not await this so the user can enter the app instantly with a loading state
+                else {
+                    setWalletState((state) => ({
+                        ...state,
+                        loading: true,
+                        hasWallet: true,
+                        isBackupDone,
+                    }));
 
-                recoverWallets(settings.network);
+                    recoverWallets(settings.network);
+                }
+
+                setSettingsState(settings);
+
+                setLoading(false);
+
+                AsyncStorage.getItem("wallet").then((value) => {
+                    Alert.alert("wallet", JSON.stringify(value));
+                });
+
+                ExpoSecureStore.getItemAsync("wallet_SECURE").then((value) => {
+                    Alert.alert("wallet_SECURE", JSON.stringify(value));
+                });
+            } catch (e) {
+                Alert.alert("Error", e instanceof Error ? e.message : JSON.stringify(e));
+                AsyncStorage.getItem("wallet").then((value) => {
+                    Alert.alert("wallet", JSON.stringify(value));
+                });
+
+                ExpoSecureStore.getItemAsync("wallet_SECURE").then((value) => {
+                    Alert.alert("wallet_SECURE", JSON.stringify(value));
+                });
             }
-
-            setSettingsState(settings);
-
-            setLoading(false);
         };
         getStorage();
     }, []);
