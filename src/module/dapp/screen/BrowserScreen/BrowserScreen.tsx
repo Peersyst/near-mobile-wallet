dc439abd18225888a48f5ee4b2b56ad5e620644b
JordiParraCrespo <jordiparra99@gmail.com>
2025-03-19 00:15:47 +0700

    fix: update intents links

--- src/module/dapp/screen/BrowserScreen/BrowserScreen.tsx
+++ src/module/dapp/screen/BrowserScreen/BrowserScreen.tsx
@@ -4,12 +4,16 @@ import BrowserScreenHeader from "./BrowserScreenHeader/BrowserScreenHeader";
 import { useEffect, useState } from "react";
 import { LoadingLogo } from "module/common/component/feedback/LoadingLogo/LoadingLogo";
 import { Col } from "@peersyst/react-native-components";
+import { Platform } from "react-native";
+import Typography from "module/common/component/display/Typography/Typography";
+import useTranslate from "module/common/hook/useTranslate";
 
 const BrowserScreen = (): JSX.Element => {
     const { headerProps, webviewProps } = useBrowserScreen();
     // Workaround to ensure the screen infers the correct height without the header
     const [isLoaded, setIsLoaded] = useState(false);
     const [isWebViewLoaded, setIsWebViewLoaded] = useState(false);
+    const translate = useTranslate();
 
     useEffect(() => {
         setTimeout(() => {
@@ -24,7 +28,20 @@ const BrowserScreen = (): JSX.Element => {
                 {isLoaded && <BrowserScreenWebView onLoadEnd={() => setIsWebViewLoaded(true)} {...webviewProps} />}
                 {(!isLoaded || !isWebViewLoaded) && (
                     <BrowserScreenLoadingContainer>
-                        <LoadingLogo />
+                        {/*    <LoadingLogo /> */}
+                        <Col alignItems="center" gap={30} justifyContent="center">
+                            <LoadingLogo />
+                            {Platform.OS === "android" && (
+                                <Col gap={4} alignItems="center">
+                                    <Typography variant="body1Strong" color="primary">
+                                        {translate("loading") + "..."}
+                                    </Typography>
+                                    <Typography variant="body3Regular" color="primary">
+                                        {translate("pleaseWaitAMoment")}
+                                    </Typography>
+                                </Col>
+                            )}
+                        </Col>
                     </BrowserScreenLoadingContainer>
                 )}
             </Col>
